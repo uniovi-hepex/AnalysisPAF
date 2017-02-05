f9597508b76cd883ee46038df14c69fd7c35c917
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2017-02-05 21:05:12 +0100

    Added package Functions

---
+++ packages/Functions/Functions.h
@@ -0,0 +1,39 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H 1
+
+#include "Lepton.h"
+#include "Jet.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// enum 
+enum iSFs{
+	iMuonReco, iMuonId, iMuonIso, iMuonIP2D, iMuonSIP3D,
+	iElecReco, iElecId, iElecIso, iElecIP2D, iElecSIP3D,
+	iTrigDoubleMuon, iTrigDoubleElec, iTrigElMu,
+	nfiles
+};
+
+enum wps{iVeto, iLoose, iMedium, iTight, iVeryTight};
+
+enum sel{iStopSelec, iOldStopSelec, iTopSelec, iWWSelec, ittDMSelec};
+
+enum sys{iNom, 
+	iJesUp, iJesDown, iJERUp, iJERDown, 
+	iBtagUp, iBtagDown, iBtagMisUp, iBtagMisDown, 
+	iLepEffUp, iLepEffDown, 
+	iTrigUp, iTrigDown, 
+	iPUUp, iPUDown, 
+	iFSUp, iFSDown,
+	nSys
+};
+
+bool LepMomentumComparator(Lepton i, Lepton j);
+bool JetMomentumComparator(Jet i   , Jet    j);
+
+vector<Lepton> SortLeptonsByPt(vector<Lepton>& Leptons);
+vector<Jet>       SortJetsByPt(vector<Jet>   & Jets);
+
+#endif
