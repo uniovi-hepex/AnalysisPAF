c9faa246b83960c22d346078470b55b3a890eb1a
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2017-03-20 23:16:24 +0100

    WW stuff, updates from Dani

--- packages/WWAnalysis/WWAnalysis.C
+++ packages/WWAnalysis/WWAnalysis.C
@@ -6,15 +6,16 @@ WWAnalysis::WWAnalysis() : PAFChainItemSelector() {
 	fTree = 0;
 
 	TrigSF = 0; TrigSF_Up = 0; TrigSF_Down = 0; PUSF = 0; PUSF_Up = 0; PUSF_Down = 0;
-	gChannel = 0; passMETfilters = 0; passTrigger = 0; isSS = 0;  NormWeight = 0; TWeight = 0;
-	TMll = 0;  TMET = 0; TMET_Phi = 0; TNJets = 0; TNBtags = 0; THT = 0; 
+	gChannel = 0; passMETfilters = 0; passTrigger = 0; TIsSS = 0;  NormWeight = 0; TWeight = 0;
+	TMll = 0;  TPtdil = 0; TMET = 0; TMET_Phi = 0; TNJets = 0; TNBtags = 0; THT = 0; 
 	TNVetoLeps = 0; TNSelLeps = 0; TChannel = 0;
 	TNJetsJESUp = 0; TNJetsJESDown = 0; TNJetsJER = 0;
 	TNBtagsUp = 0; TNBtagsDown = 0; TNBtagsMisTagUp = 0; TNBtagsMisTagDown = 0;
 	THTJESUp = 0; THTJESDown = 0; TNISRJets = 0;
-	TMETJESUp = 0; TMETJESDown = 0; TMT2llJESUp = 0; TMT2llJESDown = 0;
+	TMETJESUp = 0; TMETJESDown = 0; TMT2llJESUp = 0; TMT2llJESDown = 0; TMT2 = 0; TMT = 0; TMTprime = 0;
 	TWeight_LepEffUp = 0; TWeight_LepEffDown = 0; TWeight_TrigUp = 0; TWeight_TrigDown = 0;
-	TWeight_FSUp = 0; TWeight_FSDown = 0; TWeight_PUDown = 0; TWeight_PUUp = 0;
+	TWeight_FSUp = 0; TWeight_FSDown = 0; TWeight_PUDown = 0; TWeight_PUUp = 0;  TNVetoJets = 0;
+	TDeltaPhi = 0;
 	
 	for(Int_t i = 0; i < 10; i++){
 		TLep_Pt[i] = 0;  
@@ -78,18 +79,18 @@ void WWAnalysis::InsideLoop(){
 	gChannel        = GetParam<Int_t>("gChannel");
 	passMETfilters  = GetParam<Bool_t>("METfilters");
 	passTrigger     = GetParam<Bool_t>("passTrigger");
-	isSS            = GetParam<Bool_t>("isSS");
-
-	// Give the correct values to the variables in the mini tree
-	GetLeptonVariables(selLeptons, vetoLeptons);
-	GetJetVariables(selJets, Jets15);
+	TIsSS           = GetParam<Bool_t>("isSS");
+	
+  // Leptons and Jets
+  GetLeptonVariables(selLeptons, vetoLeptons);
+  GetJetVariables(selJets, Jets15);
 	GetMET();
 
 
 // ===================================================================================================================
 	// nivel más básico: dileptónico veto tercer lepton, Mll > 12, dos leptones, passes Trigger y MET y no cumplen mismo signo (SameSing)
 	
-	if(TNSelLeps == 2 && passTrigger && passMETfilters && !isSS){ // 2 leptons, OS
+	if(TNSelLeps == 2 && passTrigger && passMETfilters){ // 2 leptons, OS
 		if(TNVetoLeps < 3){  // Veto to 3rd lepton
 
 			// Deal with weights:
@@ -103,17 +104,26 @@ void WWAnalysis::InsideLoop(){
 			TWeight_TrigDown   = NormWeight*lepSF*TrigSF_Down*PUSF;
 			TWeight_PUDown     = NormWeight*lepSF*TrigSF*PUSF_Up;
 			TWeight_PUUp       = NormWeight*lepSF*TrigSF*PUSF_Down;
+			if(gIsData) TWeight = 1;
+
 
 			// Event Selection
 			// ===================================================================================================================
 			// utilizando minitrees aquí pongo el nivel más básico de seleccion de eventos. Será en el plotter donde escogeremos más a fondo.
-		
-			if( (((selLeptons.at(0).p +selLeptons.at(1)).p).M() > 12) // mll > 12 GeV
-			    && gChannel == 1   // eµ
-				&& TNJets < 2      //quiero 0 y 1 jets no mas
-				&& nVetoJets == 0){  // No quiero ningun b
+			
+						
+						
+						// analisis de cosas básicas de Top:
+			//if( ((selLeptons.at(0).p +selLeptons.at(1).p).M() > 20) // mll > 20 GeV
+				//&& (gChannel == 1 || (TMath::Abs((selLeptons.at(0).p + selLeptons.at(1).p).M() - 91) > 15)  )//  Z Veto in ee, µµ
+				//&& TNJets > 1      
+				//&& TNBtags > 0 ){      
+				//fTree->Fill();
+			//};
+						// analisis de cosas básicas de WW:
+			if( ((selLeptons.at(0).p +selLeptons.at(1).p).M() > 12) // mll > 12 GeV
+				&& (gChannel == 1 || (TMath::Abs((selLeptons.at(0).p + selLeptons.at(1).p).M() - 91) > 15))){ //Channel e mu y con Z Veto in ee, µµ
 				fTree->Fill();
-
 			};
 		};
 	};
@@ -133,7 +143,11 @@ void WWAnalysis::SetLeptonVariables(){
 	fTree->Branch("TLep_E" ,     TLep_E ,     "TLep_E[TNSelLeps]/F");
 	fTree->Branch("TLep_Charge",  TLep_Charge, "TLep_Charge[TNSelLeps]/F");
 	fTree->Branch("TMll",         &TMll ,       "TMll/F");
+	fTree->Branch("TPtdil",         &TPtdil ,       "TPtdil/F");
 	fTree->Branch("TChannel",      &TChannel,          "TChannel/I");
+	fTree->Branch("TMT2",      &TMT2,      "TMT2/F");
+	fTree->Branch("TMT",       &TMT,        "TMT/F");
+	fTree->Branch("TMTprime",    &TMTprime,    "TMTprime/F");
 };
 
 void WWAnalysis::SetJetVariables(){
@@ -144,7 +158,10 @@ void WWAnalysis::SetJetVariables(){
   fTree->Branch("TJet_Eta",      TJet_Eta,           "TJet_Eta[TNJets]/F");
   fTree->Branch("TJet_Phi",      TJet_Phi,           "TJet_Phi[TNJets]/F");
   fTree->Branch("TJet_E",        TJet_E,            "TJet_E[TNJets]/F");
-
+  fTree->Branch("THT",          &THT,          "THT/F");
+  fTree->Branch("TNVetoJets",     &TNVetoJets,     "TNVetoJets/I");
+  
+  if(gIsData) return;
   fTree->Branch("TNJetsJESUp",      &TNJetsJESUp,         "TNJetsJESUp/I");
   fTree->Branch("TNJetsJESDown",    &TNJetsJESDown,         "TNJetsJESDown/I");
   fTree->Branch("TNJetsJER",        &TNJetsJER,         "TNJetsJER/I");
@@ -158,22 +175,25 @@ void WWAnalysis::SetJetVariables(){
   fTree->Branch("TJetJESDown_Pt",    TJetJESDown_Pt,    "TJetJESDown_Pt[TNJetsJESDown]/F");
   fTree->Branch("TJetJER_Pt",        TJetJER_Pt,        "TJetJER_Pt[TNJetsJER]/F");
 
-  fTree->Branch("THT",          &THT,          "THT/F");
+ 
 }
 
 void WWAnalysis::SetEventVariables(){
-  fTree->Branch("TWeight",      &TWeight,      "TWeight/F");
-  fTree->Branch("TWeight_LepEffUp",      &TWeight_LepEffUp,      "TWeight_LepEffUp/F");
-  fTree->Branch("TWeight_LepEffDown",    &TWeight_LepEffDown,    "TWeight_LepEffDown/F");
-  fTree->Branch("TWeight_TrigUp",        &TWeight_TrigUp,        "TWeight_TrigUp/F");
-  fTree->Branch("TWeight_TrigDown",      &TWeight_TrigDown,      "TWeight_TrigDown/F");
-  fTree->Branch("TWeight_PUUp",        &TWeight_PUUp,        "TWeight_PUUp/F");
-  fTree->Branch("TWeight_PUDown",        &TWeight_PUDown,        "TWeight_PUDown/F");
+	fTree->Branch("TWeight",      &TWeight,      "TWeight/F");
+	fTree->Branch("TMET",         &TMET,         "TMET/F");
+	fTree->Branch("TMET_Phi",     &TMET_Phi,     "TMET_Phi/F");
+	fTree->Branch("TIsSS",        &TIsSS,        "TIsSS/B");
+	
+	if(gIsData) return;
+	fTree->Branch("TWeight_LepEffUp",      &TWeight_LepEffUp,      "TWeight_LepEffUp/F");
+	fTree->Branch("TWeight_LepEffDown",    &TWeight_LepEffDown,    "TWeight_LepEffDown/F");
+	fTree->Branch("TWeight_TrigUp",        &TWeight_TrigUp,        "TWeight_TrigUp/F");
+	fTree->Branch("TWeight_TrigDown",      &TWeight_TrigDown,      "TWeight_TrigDown/F");
+	fTree->Branch("TWeight_PUUp",        &TWeight_PUUp,        "TWeight_PUUp/F");
+	fTree->Branch("TWeight_PUDown",        &TWeight_PUDown,        "TWeight_PUDown/F");
 
-  fTree->Branch("TMET",         &TMET,         "TMET/F");
-  fTree->Branch("TMET_Phi",     &TMET_Phi,     "TMET_Phi/F");
-  fTree->Branch("TMETJESUp",    &TMETJESUp,    "TMETJESUp/F");
-  fTree->Branch("TMETJESDown",  &TMETJESDown,  "TMETJESDown/F");
+	fTree->Branch("TMETJESUp",    &TMETJESUp,    "TMETJESUp/F");
+	fTree->Branch("TMETJESDown",  &TMETJESDown,  "TMETJESDown/F");
 }
 
 void WWAnalysis::GetLeptonVariables(std::vector<Lepton> selLeptons, std::vector<Lepton> VetoLeptons){
@@ -192,11 +212,28 @@ void WWAnalysis::GetLeptonVariables(std::vector<Lepton> selLeptons, std::vector<
   else if(selLeptons.at(0).isElec && selLeptons.at(1).isMuon) TChannel = 1;
   else if(selLeptons.at(0).isMuon && selLeptons.at(1).isMuon) TChannel = 2;
   else if(selLeptons.at(0).isElec && selLeptons.at(1).isElec) TChannel = 3;
-  if(TNSelLeps > 1) TMll = (selLeptons.at(0).p + selLeptons.at(1).p).M();      
+  
+  if(TNSelLeps > 1){
+	TMll = (selLeptons.at(0).p + selLeptons.at(1).p).M();    
+	TPtdil = (selLeptons.at(0).p + selLeptons.at(1).p).Pt(); 
+	TMT2 = getMT2ll(selLeptons.at(0), selLeptons.at(1), Get<Float_t>("met_pt"), Get<Float_t>("met_phi") ); 
+	
+	
+	
+
+	TLorentzVector met;
+	met.SetPtEtaPhiM(Get<Float_t>("met_pt"), 0, Get<Float_t>("met_phi"), 0);
+	TMT = getMT(selLeptons.at(0).p + selLeptons.at(1).p, met);
+	
+	//defincion directa con el TLorentzVector:
+	TMTprime = (selLeptons.at(0).p + selLeptons.at(1).p).Mt();
+  }
+  
 }
 
 void WWAnalysis::GetJetVariables(std::vector<Jet> selJets, std::vector<Jet> cleanedJets15, Float_t ptCut){
   TNJets = selJets.size(); THT = 0;
+  TNVetoJets = nVetoJets;
   TNBtags = 0; TNBtagsUp = 0; TNBtagsDown = 0;
   TNBtagsMisTagUp = 0;  TNBtagsMisTagDown = 0;
   for(Int_t i = 0; i < TNJets; i++){
