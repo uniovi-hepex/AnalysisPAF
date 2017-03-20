0f1099dad29ba620326578944034d508402b530e
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2017-03-20 23:10:45 +0100

    Fixed important bug in Jet Selector

--- packages/JetSelector/JetSelector.C
+++ packages/JetSelector/JetSelector.C
@@ -31,7 +31,7 @@ void JetSelector::Initialise(){
 
   //---- Select your wp for b-tagging and pt, eta fot the jets
   //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  if      (gSelection == iStopSelec || iTopSelec)  stringWP = "Medium";
+  if      (gSelection == iStopSelec || gSelection == iTopSelec)  stringWP = "Medium";
   else if (gSelection == iWWSelec)    stringWP = "Loose";
   else                                stringWP = "Medium";
   if     (gSelection == iStopSelec || iTopSelec){
@@ -135,6 +135,7 @@ void JetSelector::InsideLoop(){
     }
   }
   if(jet_MaxEta > 2.4){ // Add jets from JetFwd collection
+    cout << "Adding Fwd jet..." << endl;
     nJet = Get<Int_t>("nJetFwd");
     for(Int_t i = 0; i < nJet; i++){
       GetJetFwdVariables(i);
@@ -191,7 +192,7 @@ void JetSelector::InsideLoop(){
 Bool_t JetSelector::IsBtag(Jet j){
   Bool_t isbtag;
   if(gIsData) isbtag = fBTagSFnom->IsTagged(j.csv, -999999, j.p.Pt(), j.p.Eta());
-  else if(stringWP == "Loose") isbtag = fBTagSFnom->IsTagged(j.csv, -999999, j.p.Pt(), j.p.Eta());
+  else if(stringWP == "Loose") isbtag = Get<Float_t>("Jet_csv");//fBTagSFnom->IsTagged(j.csv, -999999, j.p.Pt(), j.p.Eta());
   else        isbtag = fBTagSFnom->IsTagged(j.csv,j.flavmc, j.p.Pt(), j.p.Eta());
   return isbtag;
 }
