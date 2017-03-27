c2d729cd367ed6d63656c87114c9edd8d22f40fa
Adeik <victoregoslorelua@gmail.com>
2017-03-27 21:44:17 +0200

    Implemented new way of execution: instead of writing the number of cores, now it's possible to write opt (without ) and a complete analysis will be done with specific number of cores for each sample.

--- plotter/ttH/DottHAnalysis.sh
+++ plotter/ttH/DottHAnalysis.sh
@@ -17,6 +17,56 @@ if [ "$1" == "an" ]; then
     cd ../..
     if [ "$2" == "test" ]; then
         root -l -b -q "RunAnalyserPAF.C(\"TestHeppy\"						            , \"ttH\", 1)"
+    elif [ "$2" == "opt" ]; then
+    	echo "%%%%%> MC SAMPLES FOR COMPARISON WITH DATA"
+        root -l -b -q "RunAnalyserPAF.C(\"TTWToQQ\"								          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TTZToLLNuNu_ext\"						      , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"TTZToLLNuNu_ext2\"					       , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TTZToQQ\"								          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WGToLNuG\"							          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"ZGTo2LG\"								          , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"TGJets\"								           , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TGJets_ext\"							        , \"ttH\", 45)"
+        #root -l -b -q "RunAnalyserPAF.C(\"TTGJets\"								         , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TTGJets_ext\"							        , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WpWpJJ\"								          , \"ttH\", 35)"
+        root -l -b -q "RunAnalyserPAF.C(\"ZZZ\"									            , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WWZ\"									            , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WZZ\"									            , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"WW\"									             , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WW_ext\"								          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"tZq_ll\"								          , \"ttH\", 55)"
+        root -l -b -q "RunAnalyserPAF.C(\"TTTT\"								            , \"ttH\", 35)"
+
+		echo "%%%%%> MC SAMPLES FOR CONTROL REGIONS"
+        root -l -b -q "RunAnalyserPAF.C(\"TTJets_aMCatNLO\"						      , \"ttH\", 50)"
+        root -l -b -q "RunAnalyserPAF.C(\"DYJetsToLL_M50_aMCatNLO\"				  , \"ttH\", 64)"
+        root -l -b -q "RunAnalyserPAF.C(\"DYJetsToLL_M10to50_aMCatNLO_ext\" , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WJetsToLNu_MLM\"						      , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"WJetsToLNu_MLM_ext2\"					   , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"TW\"									             , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TW_ext\"								          , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"TbarW\"								           , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TbarW_ext\"							          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"T_tch\"								            , \"ttH\", 40)"
+        root -l -b -q "RunAnalyserPAF.C(\"Tbar_tch\"							          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"TToLeptons_sch_amcatnlo\"				  , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WZTo3LNu\"							          , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WWTo2L2Nu\"							          , \"ttH\", 25)"
+        #root -l -b -q "RunAnalyserPAF.C(\"ZZ\"									             , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"ZZ_ext\"								          , \"ttH\", 25)"
+
+		echo "%%%%%> MC SIGNAL SAMPLES"
+        root -l -b -q "RunAnalyserPAF.C(\"TTHNonbb\"                       	, \"ttH\", 25)"
+
+		echo "%%%%%> DATA SAMPLES"
+        root -l -b -q "RunAnalyserPAF.C(\"MuonEG\"                          , \"ttH\", 64)"
+        root -l -b -q "RunAnalyserPAF.C(\"DoubleMuon\"                      , \"ttH\", 64)"
+        root -l -b -q "RunAnalyserPAF.C(\"DoubleEG\"                        , \"ttH\", 64)"
+        root -l -b -q "RunAnalyserPAF.C(\"SingleElec\"                    	, \"ttH\", 64)"
+        root -l -b -q "RunAnalyserPAF.C(\"SingleMuon\"                      , \"ttH\", 64)"
+        
+        
     else
     	echo "%%%%%> MC SAMPLES FOR COMPARISON WITH DATA"
         root -l -b -q "RunAnalyserPAF.C(\"TTWToLNu_ext2\"						        , \"ttH\", $2)"
@@ -31,13 +81,13 @@ if [ "$1" == "an" ]; then
         #root -l -b -q "RunAnalyserPAF.C(\"TTGJets\"								         , \"ttH\", $2)"
         root -l -b -q "RunAnalyserPAF.C(\"TTGJets_ext\"							        , \"ttH\", $2)"
         root -l -b -q "RunAnalyserPAF.C(\"WpWpJJ\"								          , \"ttH\", $2)"
-        root -l -b -q "RunAnalyserPAF.C(\"ZZZ\"									            , \"ttH\", $2)"	# Rare
-        root -l -b -q "RunAnalyserPAF.C(\"WWZ\"									            , \"ttH\", $2)"	# Rare
-        root -l -b -q "RunAnalyserPAF.C(\"WZZ\"									            , \"ttH\", $2)"	# Rare
-        #root -l -b -q "RunAnalyserPAF.C(\"WW\"									             , \"ttH\", $2)"	# Rare
-        root -l -b -q "RunAnalyserPAF.C(\"WW_ext\"								          , \"ttH\", $2)"	# Rare
-        root -l -b -q "RunAnalyserPAF.C(\"tZq_ll\"								          , \"ttH\", $2)"	# Rare
-        root -l -b -q "RunAnalyserPAF.C(\"TTTT\"								            , \"ttH\", $2)"	# Rare
+        root -l -b -q "RunAnalyserPAF.C(\"ZZZ\"									            , \"ttH\", $2)"
+        root -l -b -q "RunAnalyserPAF.C(\"WWZ\"									            , \"ttH\", $2)"
+        root -l -b -q "RunAnalyserPAF.C(\"WZZ\"									            , \"ttH\", $2)"
+        #root -l -b -q "RunAnalyserPAF.C(\"WW\"									             , \"ttH\", $2)"
+        root -l -b -q "RunAnalyserPAF.C(\"WW_ext\"								          , \"ttH\", $2)"
+        root -l -b -q "RunAnalyserPAF.C(\"tZq_ll\"								          , \"ttH\", $2)"
+        root -l -b -q "RunAnalyserPAF.C(\"TTTT\"								            , \"ttH\", $2)"
 
 		echo "%%%%%> MC SAMPLES FOR CONTROL REGIONS"
         root -l -b -q "RunAnalyserPAF.C(\"TTJets_aMCatNLO\"						      , \"ttH\", $2)"
