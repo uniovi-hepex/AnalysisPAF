a3860f83633a4919c0092711b49a71fca598b4dd
Adeik <victoregoslorelua@gmail.com>
2017-03-28 10:54:49 +0200

    Added to time/ef records, added new sample (WWW) and changed ncores in the optimum option

--- plotter/ttH/DottHAnalysis.sh
+++ plotter/ttH/DottHAnalysis.sh
@@ -22,45 +22,46 @@ if [ "$1" == "an" ]; then
     elif [ "$2" == "opt" ]; then
     	echo "%%%%%> MC SAMPLES FOR COMPARISON WITH DATA"
         root -l -b -q "RunAnalyserPAF.C(\"TTWToQQ\"								             , \"ttH\", 25)"
-        root -l -b -q "RunAnalyserPAF.C(\"TTZToLLNuNu_ext & TTZToLLNuNu_ext2\" , \"ttH\", 40)" #25 - 60s
+        root -l -b -q "RunAnalyserPAF.C(\"TTZToLLNuNu_ext & TTZToLLNuNu_ext2\" , \"ttH\", 64)" #25 - 60s, 40 - 57s
         root -l -b -q "RunAnalyserPAF.C(\"TTZToQQ\"								             , \"ttH\", 25)"
         root -l -b -q "RunAnalyserPAF.C(\"WGToLNuG\"							             , \"ttH\", 25)"
-        root -l -b -q "RunAnalyserPAF.C(\"ZGTo2LG\"								             , \"ttH\", 50)" #25 - 80s
-        root -l -b -q "RunAnalyserPAF.C(\"TGJets & TGJets_ext\"							   , \"ttH\", 45)"
+        root -l -b -q "RunAnalyserPAF.C(\"ZGTo2LG\"								             , \"ttH\", 64)" #25 - 80s, 50 - 65
+        root -l -b -q "RunAnalyserPAF.C(\"TGJets & TGJets_ext\"							   , \"ttH\", 55)" # 45 - 34s
         root -l -b -q "RunAnalyserPAF.C(\"TTGJets_ext\"							 , \"ttH\", 30)" # 25 - 35s
         #root -l -b -q "RunAnalyserPAF.C(\"TTGJets & TTGJets_ext\"							 , \"ttH\", 25)"
         root -l -b -q "RunAnalyserPAF.C(\"WpWpJJ\"								             , \"ttH\", 35)" # petó
         root -l -b -q "RunAnalyserPAF.C(\"ZZZ\"									               , \"ttH\", 25)"
-        root -l -b -q "RunAnalyserPAF.C(\"WWZ\"									               , \"ttH\", 25)"
         root -l -b -q "RunAnalyserPAF.C(\"WZZ\"									               , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WWZ\"									               , \"ttH\", 25)"
+        root -l -b -q "RunAnalyserPAF.C(\"WWW\"									               , \"ttH\", 25)"
         root -l -b -q "RunAnalyserPAF.C(\"WW_ext\"								         , \"ttH\", 25)"
         #root -l -b -q "RunAnalyserPAF.C(\"WW & WW_ext\"								         , \"ttH\", 25)"
         root -l -b -q "RunAnalyserPAF.C(\"tZq_ll\"								             , \"ttH\", 64)" # 55 - 122s
-        root -l -b -q "RunAnalyserPAF.C(\"TTTT\"								               , \"ttH\", 45)" # 35 - 38s
+        root -l -b -q "RunAnalyserPAF.C(\"TTTT\"								               , \"ttH\", 30)" # 35 - 38s, 45 - 40s
 
 		echo "%%%%%> MC SAMPLES FOR CONTROL REGIONS"
-        root -l -b -q "RunAnalyserPAF.C(\"TTJets_aMCatNLO\"						         , \"ttH\", 64)" # 50 - 69s
+        root -l -b -q "RunAnalyserPAF.C(\"TTJets_aMCatNLO\"						         , \"ttH\", 40)" # 50 - 69s, 64 - 74s
         root -l -b -q "RunAnalyserPAF.C(\"DYJetsToLL_M50_aMCatNLO\"				     , \"ttH\", 64)" # 64 - 312s
-        root -l -b -q "RunAnalyserPAF.C(\"DYJetsToLL_M10to50_aMCatNLO & DYJetsToLL_M10to50_aMCatNLO_ext\" , \"ttH\", 30)" # 25 - 21s
+        root -l -b -q "RunAnalyserPAF.C(\"DYJetsToLL_M10to50_aMCatNLO & DYJetsToLL_M10to50_aMCatNLO_ext\" , \"ttH\", 20)" # 25 - 21s, 30 - 22s
         root -l -b -q "RunAnalyserPAF.C(\"WJetsToLNu_aMCatNLO\"						     , \"ttH\", 25)"
-        root -l -b -q "RunAnalyserPAF.C(\"TW & TW_ext\"								         , \"ttH\", 30)" # 25 - 24s
-        root -l -b -q "RunAnalyserPAF.C(\"TbarW & TbarW_ext\"							     , \"ttH\", 30)" # 25 - 25s
-        root -l -b -q "RunAnalyserPAF.C(\"T_tch\"								               , \"ttH\", 50)" # 40 - 35s
-        root -l -b -q "RunAnalyserPAF.C(\"Tbar_tch\"							             , \"ttH\", 35)" # 25 - 27s
+        root -l -b -q "RunAnalyserPAF.C(\"TW & TW_ext\"								         , \"ttH\", 20)" # 25 - 24s, 30 - 26s
+        root -l -b -q "RunAnalyserPAF.C(\"TbarW & TbarW_ext\"							     , \"ttH\", 20)" # 25 - 25s, 30 - 26s
+        root -l -b -q "RunAnalyserPAF.C(\"T_tch\"								               , \"ttH\", 64)" # 40 - 35s, 50 - 36s
+        root -l -b -q "RunAnalyserPAF.C(\"Tbar_tch\"							             , \"ttH\", 45)" # 25 - 27s, 35 - 26s
         root -l -b -q "RunAnalyserPAF.C(\"TToLeptons_sch_amcatnlo\"				     , \"ttH\", 25)"
-        root -l -b -q "RunAnalyserPAF.C(\"WZTo3LNu\"							             , \"ttH\", 30)" # 25 - 24s
-        root -l -b -q "RunAnalyserPAF.C(\"WWTo2L2Nu\"							             , \"ttH\", 30)" # 25 - 22s
+        root -l -b -q "RunAnalyserPAF.C(\"WZTo3LNu\"							             , \"ttH\", 20)" # 25 - 24s, 30 - 25s
+        root -l -b -q "RunAnalyserPAF.C(\"WWTo2L2Nu\"							             , \"ttH\", 20)" # 25 - 22s, 20 - 24s
         root -l -b -q "RunAnalyserPAF.C(\"ZZ_ext\"								         , \"ttH\", 25)"
         #root -l -b -q "RunAnalyserPAF.C(\"ZZ & ZZ_ext\"								         , \"ttH\", 25)"
 
 		echo "%%%%%> MC SIGNAL SAMPLES"
-        root -l -b -q "RunAnalyserPAF.C(\"TTHNonbb\"                       	   , \"ttH\", 35)" # 25 - 33s
+        root -l -b -q "RunAnalyserPAF.C(\"TTHNonbb\"                       	   , \"ttH\", 50)" # 25 - 33s, 35 - 33s
 
 		echo "%%%%%> DATA SAMPLES"
         root -l -b -q "RunAnalyserPAF.C(\"MuonEG\"                             , \"ttH\", 64)" #~100s
         root -l -b -q "RunAnalyserPAF.C(\"DoubleMuon\"                         , \"ttH\", 64)" #~246s
         root -l -b -q "RunAnalyserPAF.C(\"DoubleEG\"                           , \"ttH\", 64)" #~187s
-        root -l -b -q "RunAnalyserPAF.C(\"SingleElec\"                    	   , \"ttH\", 64)" #~158s
+        root -l -b -q "RunAnalyserPAF.C(\"SingleElec\"                    	   , \"ttH\", 64)" #~160s
         root -l -b -q "RunAnalyserPAF.C(\"SingleMuon\"                         , \"ttH\", 64)" #~227s
 
     else
