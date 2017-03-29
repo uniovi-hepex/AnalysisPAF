02bc34761b191bc443d10a728d509b74fc7e3289
Adeik <victoregoslorelua@gmail.com>
2017-03-30 01:03:10 +0200

    Implemented taus within Lepton class.

--- packages/Lepton/Lepton.h
+++ packages/Lepton/Lepton.h
@@ -10,7 +10,7 @@ class Lepton : public TObject{
       p = TLorentzVector(0,0,0,0);
       charge = 0;
       type = -1;
-      isElec = 0; isMuon = 0;
+      isElec = 0; isMuon = 0; isTau = 0;
     };
     //Lepton(const Lepton &l): p(l.p), charge(l.charge), type(l.type), index(l.index){ };
     Lepton(TLorentzVector vec, Int_t ch = 0, Int_t ty = -1){
@@ -18,8 +18,9 @@ class Lepton : public TObject{
       charge = ch;
       type = ty;
 
-      if(ty == 0){ isMuon = 1; isElec = 0;}
-      if(ty == 1){ isMuon = 0; isElec = 1;}
+      if(ty == 0){ isMuon = 1; isElec = 0; isTau = 0;}
+      if(ty == 1){ isMuon = 0; isElec = 1; isTau = 0;}
+      if(ty == 2){ isMuon = 0; isElec = 0; isTau = 1;}
     }
     ~Lepton(){};
     TLorentzVector p;
@@ -27,6 +28,7 @@ class Lepton : public TObject{
     Int_t type;
     Bool_t isElec;
     Bool_t isMuon;
+    Bool_t isTau;
     Float_t GetSF(Int_t sys = 0);
     void SetSF(Float_t val);
     void SetSFerr(Float_t val);
