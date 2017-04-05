addc1d5c57691ae3d23aa1b782e2ff6529f4e7ec
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2017-04-05 15:16:37 +0200

    Only Stop 2l

--- plotter/Stop2L/MakeDatacards.C
+++ plotter/Stop2L/MakeDatacards.C
@@ -1,6 +1,7 @@
 R__LOAD_LIBRARY(Histo.C+)
 R__LOAD_LIBRARY(Looper.C+)
 R__LOAD_LIBRARY(Plot.C+)
+R__LOAD_LIBRARY(TResultsTable.C+)
 #include "Histo.h"
 #include "Looper.h"
 #include "Plot.h"
@@ -14,9 +15,9 @@ TString BaselineCut = "TMET > 50 && TNJets > 1 && TNBtags > 0 && !TIsSS && TNVet
 void MakeDatacards(){
   //DrawPlot("TChannel", BaselineCut, "ElMu", 1, 0, 2, "Channel", "CutAndCount");
   //DrawPlot("TMT2", BaselineCut, "ElMu", 40, 0, 200, "M_{T2} [GeV]");
-      DrawPlot("TMT2", "TMET > 50 && TNJets > 1 && TNBtags > 0 && !TIsSS && TNVetoLeps < 3 && TMT2 < 140", "ElMu", 40, 0, 200, "M_{T2} [GeV]", "TMT2low"); //  DrawPlot("TMET", "TNJets > 1 && TNBtags > 0 && !TIsSS && TNVetoLeps < 3", "ElMu", 40, 0, 400, "MET [GeV]");
-//  DrawPlot("TDeltaPhi", BaselineCut, "ElMu", 30, -3.15, 3.15, "#Delta#varphi_{e#mu} [rad]");
-//  DrawPlot("TDeltaEta", BaselineCut, "ElMu", 30, 0, 2.4, "#Delta#eta_{e#mu} [rad]");
+//      DrawPlot("TMT2", "TMET > 50 && TNJets > 1 && TNBtags > 0 && !TIsSS && TNVetoLeps < 3 && TMT2 < 140", "ElMu", 40, 0, 200, "M_{T2} [GeV]", "TMT2low"); //  DrawPlot("TMET", "TNJets > 1 && TNBtags > 0 && !TIsSS && TNVetoLeps < 3", "ElMu", 40, 0, 400, "MET [GeV]");
+  DrawPlot("TDeltaPhi", BaselineCut, "ElMu", 30, -3.15, 3.15, "#Delta#varphi_{e#mu} [rad]");
+  //DrawPlot("TDeltaEta", BaselineCut, "ElMu", 30, 0, 2.4, "#Delta#eta_{e#mu} [rad]");
 //  DrawPlot("TChannel", BaselineCut, "All", 3, 1, 4, "Channel", "Comb3chan");
   //DrawPlot("TNJets", BaselineCut, "ElMu", 1, 0, 10, "CutAndCount","CutAndCount");
   //DrawPlot("TMT2", BaselineCut, "ElMu", 40, 0, 200, "M_{T2} [GeV]");
@@ -55,8 +56,8 @@ void DrawPlot(TString var, TString cut, TString chan, Int_t nbins, Float_t bin0,
   p->AddSample("TTbar_Powheg_ueDown", "ttbar", itSys, 1, 0, "ueDown"); 
   p->AddSample("TTbar_Powheg_isrUp", "ttbar", itSys, 1, 0, "isrUp"); 
   p->AddSample("TTbar_Powheg_isrDown", "ttbar", itSys, 1, 0, "isrDown"); 
-//  p->AddSample("TTbar_Powheg_fsrUp", "ttbar", itSys, 1, 0, "fsrUp"); 
-//  p->AddSample("TTbar_Powheg_fsrDown", "ttbar", itSys, 1, 0, "fsrDown"); 
+  p->AddSample("TTbar_Powheg_fsrUp", "ttbar", itSys, 1, 0, "fsrUp"); 
+  p->AddSample("TTbar_Powheg_fsrDown", "ttbar", itSys, 1, 0, "fsrDown"); 
   p->AddSample("TTJets_aMCatNLO", "ttbar", itSys, 1, 0, "nloUp"); 
   p->AddSample("TTbar_PowhegLHE", "ttbar", itSys, 1, 0, "nloDown"); 
   p->AddSample("TTbar_Powheg_Herwig", "ttbar", itSys, 1, 0, "hadUp"); 
@@ -70,16 +71,15 @@ void DrawPlot(TString var, TString cut, TString chan, Int_t nbins, Float_t bin0,
   p->AddSample("T2tt_mStop225_mLsp50", "S_225_50", itSignal, kCyan, 0.1); 
   p->AddSample("T2tt_mStop242_mLsp75", "S_242_75", itSignal, kCyan, 0.1); 
   p->AddSample("T2tt_mStop250_mLsp75", "S_250_75", itSignal, kCyan, 0.1); 
-  p->AddSample("T2tt_mStop258_mLsp75", "S_258_75", itSignal, kCyan, 0.1); 
   p->AddSample("T2tt_200_50_FS_summer", "SFS_200_50", itSignal, kCyan, 0.1); 
   p->AddSample("T2tt_225_50_FS_summer", "SFS_225_50", itSignal, kPink, 0.1); 
   p->AddSample("T2tt_250_50_FS_summer", "SFS_250_50", itSignal, 1, 0.1); 
 
-  p->AddSystematic("JES,Btag,MisTag,LepEff,PU");
+  p->AddSystematic("JES,Btag,MisTag,LepEff,PU,Scale,PDF");
 //  p->AddSystematic("Btag,MisTag,PU");
 
-  p->PrintYields();
   p->PrintSystYields();
+  p->PrintYields("","","","tex,html,txt");
   p->MakeAllDatacards();
   delete p;
 }
