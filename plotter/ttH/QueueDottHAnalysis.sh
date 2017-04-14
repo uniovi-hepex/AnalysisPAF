e8144306ba4c9a72ea37ca400a432f13abf46cdb
Adeik <victoregoslorelua@gmail.com>
2017-04-14 18:49:03 +0200

    Added bash file for queue system executions

---
+++ plotter/ttH/QueueDottHAnalysis.sh
@@ -0,0 +1,23 @@
+#===============================================================================
+#
+#                         Analysis of the ttH process
+#
+#===============================================================================
+if [ "$1" == "an" ]; then
+    echo ""
+    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH ANALYSIS EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    echo ""
+    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating job..."
+    qsub -q proof -l nodes=1:ppn=$2 -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normal_o_\$TASK_ID.txt -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normal_e_\$TASK_ID.txt -F "an $2 $3" DottHAnalysis.sh
+    fi
+    cd plotter/ttH
+elif [ "$1" == "pl" ]; then
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH PLOTTER EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating jobs..."
+
+else
+    echo "ERROR - No valid arguments given"
+    echo "Please, execute this script with a valid argument"
+fi
