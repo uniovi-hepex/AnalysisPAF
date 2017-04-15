dfa4da41042ae005789fa281566324a5571187bb
Adeik <victoregoslorelua@gmail.com>
2017-04-15 18:37:19 +0200

    Debugging

--- plotter/ttH/PlotThings.sh
+++ plotter/ttH/PlotThings.sh
@@ -3,7 +3,7 @@ source /cms/slc6_amd64_gcc530/external/python/2.7.11-giojec2/etc/profile.d/init.
 source /cms/slc6_amd64_gcc530/external/python/2.7.11-giojec2/etc/profile.d/dependencies-setup.sh
 source /cms/slc6_amd64_gcc530/external/cmake/3.5.2/etc/profile.d/init.sh
 source /opt/root6/bin/thisroot.sh
-cd ..
+cd /nfs/fanae/user/vrbouza/Documents/TFG/AnalysisPAF/plotter
 root -l -b -q "ttH/DrawPlots.C(\"Muon\" , \"$1\")"
 root -l -b -q "ttH/DrawPlots.C(\"Elec\" , \"$1\")"
 root -l -b -q "ttH/DrawPlots.C(\"ElMu\" , \"$1\")"
@@ -11,4 +11,3 @@ root -l -b -q "ttH/DrawPlots.C(\"2lSS\" , \"$1\")"
 root -l -b -q "ttH/DrawPlots.C(\"3l\"   , \"$1\")"
 root -l -b -q "ttH/DrawPlots.C(\"4l\"   , \"$1\")"
 root -l -b -q "ttH/DrawPlots.C(\"All\"  , \"$1\")"
-cd ttH
--- plotter/ttH/QueueDottHAnalysis.sh
+++ plotter/ttH/QueueDottHAnalysis.sh
@@ -4,18 +4,18 @@
 #
 #===============================================================================
 if [ "$1" == "an" ]; then
-    echo ""
-    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH ANALYSIS EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-    echo ""
-    echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating job..."
-    qsub -q proof -l nodes=1:ppn=$2 -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normal_o_\$TASK_ID.txt -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normal_e_\$TASK_ID.txt -F "an $2 $3" DottHAnalysis.sh
-    cd plotter/ttH
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH ANALYSIS EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+  echo ""
+  echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating job..."
+  qsub -q proof -l nodes=1:ppn=$2 -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normal_o_\$TASK_ID.txt -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normal_e_\$TASK_ID.txt -F "an $2 $3" DottHAnalysis.sh
+  cd plotter/ttH
 elif [ "$1" == "pl" ]; then
   echo ""
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH PLOTTER EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
   echo ""
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating job..."
-  qsub -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normalplots_o_\$TASK_ID -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/normalplots_e_\$TASK_ID -F "0" PlotThings.sh
+  qsub -o /nfs/fanae/user/vrbouza/Documents/TFG/Executions/NORMALlogs -e /nfs/fanae/user/vrbouza/Documents/TFG/Executions/NORMALlogs -F "0" PlotThings.sh
 else
     echo "ERROR - No valid arguments given"
     echo "Please, execute this script with a valid argument"
