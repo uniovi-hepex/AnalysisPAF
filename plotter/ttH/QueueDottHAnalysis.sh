3aa679868ee91b9652466d237dd2ef503a5fe66a
Adeik <victoregoslorelua@gmail.com>
2017-04-20 19:55:29 +0200

    Minor debugging

--- plotter/ttH/QueueDottHAnalysis.sh
+++ plotter/ttH/QueueDottHAnalysis.sh
@@ -17,7 +17,6 @@ if [ "$1" == "an" ]; then
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating job..."
   An=$(qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -F "an $2 $3" DottHAnalysis.sh)
   qsub -q proof -l nodes=1:ppn=$2 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$An -F "$2 $3" check.sh
-  cd plotter/ttH
 elif [ "$1" == "pl" ]; then
   echo ""
   echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ttH PLOTTER EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
