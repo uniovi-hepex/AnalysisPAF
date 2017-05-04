20ea9a19ee02860f5ed1fdb7aeff0f3e8e933284
Pietro Vischia <pietro.vischia@gmail.com>
2017-05-04 11:18:17 +0200

    Running script

--- plotter/t4/doAnal.sh
+++ plotter/t4/doAnal.sh
@@ -2,6 +2,13 @@
 
 # Everything is gonna be run from outside the plotter
 
-# Analysis: sh plotter/t4/batch.sh
+if [ "$1" == "batch" ]; then
+    source plotter/t4/batch.sh
 
-# Tables: root -l -b plotter/t4/yields.C
+elif [ "$1" == "yield" ]; then    
+    root -l -b plotter/t4/yields.C
+
+else
+    echo "Es que no me hás dicho que hacer, así que mejor si me duermo."
+
+fi
