a5174ede739204d12ecd20774b494b1f72323010
Pietro Vischia <pietro.vischia@gmail.com>
2017-05-04 11:10:08 +0200

    Added submitter and yields printer for tttt

---
+++ plotter/t4/anal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Everything is gonna be run from outside the plotter
+
+# Analysis: sh plotter/t4/batch.sh
+
+# Tables: root -l -b plotter/t4/yields.C
---
+++ plotter/t4/batch.sh
@@ -0,0 +1,3 @@
+# Run me from outside the plotter.
+
+qsub -q proof -l nodes=1:ppn=20 plotter/t4/submit.sh
