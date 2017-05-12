3926bb1a08e040f8d9c39351bf73bacba343fb3e
Pietro Vischia <pietro.vischia@gmail.com>
2017-05-12 18:21:47 +0200

    Added variables and tttt to tau selector, and fixed tabs and indentation

--- packages/TauSelector/TauSelector.h
+++ packages/TauSelector/TauSelector.h
@@ -9,52 +9,53 @@
 #include "LinkDef.h"
 
 class TauSelector : public PAFChainItemSelector{
-
-  public:
-
-    TauSelector();
-    virtual ~TauSelector() {}
-    virtual void InsideLoop();
-    virtual void Initialise();
-    virtual void Summary();
-
-    std::vector<Lepton> selTaus;
-    std::vector<Lepton> looseTaus;
-    std::vector<Lepton> vetoTaus;
-
-    std::vector<Lepton> looseLeptons;
-
-  protected:
-
-    LeptonSF  *TauSF;
-    Bool_t 	  gIsData;
-    Int_t 	  gSelection;
-    TString   localPath;
-
-    // TauGood
-    Lepton tL;
-    Int_t   nLep;
-    TLorentzVector tP;
-    Float_t pt;
-    Float_t eta;
-    Int_t 	charge;
-    Int_t 	type;
-	  Int_t 	idDecayMode;
-    Int_t	  idMVA;
-    Int_t	  idAntiE;
-    Int_t	  idAntiMu;
-
-	  Int_t   nTau;
-
-    Int_t   nSelTaus;
-    Int_t   nVetoTaus;
-    Int_t   nLooseTaus;
-
-    void GetTauVariables(Int_t i);
-
-    Bool_t isGoodTau(Lepton tau);
-    Bool_t isLooseTau(Lepton tau);
-    Bool_t isVetoTau(Lepton tau);
-
+  
+ public:
+  
+  TauSelector();
+  virtual ~TauSelector() {}
+  virtual void InsideLoop();
+  virtual void Initialise();
+  virtual void Summary();
+  
+  std::vector<Lepton> selTaus;
+  std::vector<Lepton> looseTaus;
+  std::vector<Lepton> vetoTaus;
+  
+  std::vector<Lepton> looseLeptons;
+  
+ protected:
+  
+  LeptonSF* TauSF;
+  Bool_t    gIsData;
+  Int_t     gSelection;
+  TString   localPath;
+  
+  // TauGood
+  Lepton tL;
+  Int_t   nLep;
+  TLorentzVector tP;
+  Float_t pt;
+  Float_t eta;
+  Int_t type;
+  Int_t charge;
+  Int_t decayMode;
+  Int_t idDecayMode;
+  Int_t idMVA;
+  Int_t idAntiE;
+  Int_t idAntiMu;
+  
+  Int_t nTau;
+  
+  Int_t nSelTaus;
+  Int_t nVetoTaus;
+  Int_t nLooseTaus;
+  
+  void GetTauVariables(Int_t i);
+  
+  Bool_t isGoodTau(Lepton tau);
+  Bool_t isLooseTau(Lepton tau);
+  Bool_t isVetoTau(Lepton tau);
+  
   ClassDef(TauSelector, 0);
 };
