f7de584c514447c84b6065c17107b8948aa0e551
Pietro Vischia <pietro.vischia@gmail.com>
2017-05-17 18:30:08 +0200

    [t4] Lista de muestras con division sencilla

--- plotter/t4/t4MiniTrees.sh
+++ plotter/t4/t4MiniTrees.sh
@@ -8,7 +8,7 @@ root -l -b -q 'RunAnalyserPAF.C("DYJetsToLL_M5to50_MLM"                         
 root -l -b -q 'RunAnalyserPAF.C("DYJetsToLL_M50_MLM_ext & DYJetsToLL_M50_MLM_ext2", "4t", 50, -8)'
 
 # ttbar, single-t
-root -l -b -q 'RunAnalyserPAF.C("TTbar_Powheg"           ,"4t", 30, -4)'
+root -l -b -q 'RunAnalyserPAF.C("TTbar_Powheg"           ,"4t", 30)'
 root -l -b -q 'RunAnalyserPAF.C("T_tch"                  ,"4t", 30)'
 root -l -b -q 'RunAnalyserPAF.C("Tbar_tch"               ,"4t", 30)'
 root -l -b -q 'RunAnalyserPAF.C("TToLeptons_sch_amcatnlo","4t", 30)'
@@ -35,7 +35,7 @@ root -l -b -q 'RunAnalyserPAF.C("WWTo2L2Nu"   ,"4t", 30)' #root -l -b -q 'RunAna
 root -l -b -q 'RunAnalyserPAF.C("ZZZ"      ,"4t",  8, -2)'
 root -l -b -q 'RunAnalyserPAF.C("WZZ"      ,"4t",  8, -2)'
 root -l -b -q 'RunAnalyserPAF.C("WWZ"      ,"4t",  8, -2)'
-root -l -b -q 'RunAnalyserPAF.C("WWW"      ,"4t",  8, -2)'
+root -l -b -q 'RunAnalyserPAF.C("WWW"      ,"4t",  8)'
 root -l -b -q 'RunAnalyserPAF.C("WpWpJJ"   ,"4t", 30, -4)'
 root -l -b -q 'RunAnalyserPAF.C("VHToNonbb_amcatnlo"   ,"4t", 5)' # VHToNonbb M125 13TeV amcatnloFXFX madspin pythia8
 root -l -b -q 'RunAnalyserPAF.C("WWTo2L2Nu_DoubleScat"   ,"4t", 5)' # WW DoubleScattering 13TeV-pythia8
@@ -48,7 +48,7 @@ root -l -b -q 'RunAnalyserPAF.C("TTGJets & TTGJets_ext", "4t",8, -4)'
 root -l -b -q 'RunAnalyserPAF.C("WGToLNuG"             , "4t",8, -4)'
 root -l -b -q 'RunAnalyserPAF.C("ZGTo2LG"              , "4t",8, -4)'
 root -l -b -q 'RunAnalyserPAF.C("WZG_amcatnlo"   , "4t",8, -4)' # WZG_TuneCUETP8M1_13TeV-amcatnlo-pythia8
-root -l -b -q 'RunAnalyserPAF.C("WWG_amcatnlo"   , "4t",8, -4)' # WWG_TuneCUETP8M1_13TeV-amcatnlo-pythia8
+root -l -b -q 'RunAnalyserPAF.C("WWG_amcatnlo"   , "4t",8)' # WWG_TuneCUETP8M1_13TeV-amcatnlo-pythia8
 
 # ====================
 # SIGNAL
