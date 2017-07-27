f9713745f4cdd1c40b086c0a929e08ba115aa49c
Carlos Erice <cericecid@gmail.com>
2017-07-27 19:49:37 +0200

    Added files for batch submission

---
+++ plotter/WZ/doAnal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Everything is gonna be run from outside the plotter
+
+if [ "$1" == "batch" ]; then
+    source plotter/WZ/batch.sh
+
+elif [ "$1" == "yields" ]; then    
+    root -l -b plotter/WZ/yields.C\(\"~/www/t4/plots\"\)
+elif [ "$1" == "plots" ]; then
+    cd plotter
+    #root -l -b t4/DrawPlots.C\(\"CRT\"\)
+    root -l -b t4/DrawPlots.C\(\"SR9\"\)
+    root -l -b t4/DrawPlots.C\(\"SR10\"\)
+    cd -
+else
+    echo "Es que no me hás dicho que hacer, así que mejor si me duermo."
+
+fi
