91cccb4841dc6397cbc7a3339312f42a5e03de01
Carlos Erice <cericecid@gmail.com>
2017-09-29 16:09:00 +0200

    Replaced commented lines closing up to final plotter

--- plotter/WZ/DrawPlots.C
+++ plotter/WZ/DrawPlots.C
@@ -47,26 +47,26 @@ void DrawPlot(TString var, TString cut, TString chan, Int_t nbins, Float_t bin0,
   
 
   if (NameOfTree == "top"){//Load MC FAKES
-    p->SetCut(cut + " && TConvNumber > 0 && TFakeNumber == 0");
+    p->SetCut(cut + " && TConvNumber > 0 && TFakeNumber == 0"); //Allow Conversions
     p->AddSample("TGJets, TTGJets, WGToLNuG, ZGTo2LG, WZG_amcatnlo, WWG_amcatnlo",                "X+#gamma", itBkg, kViolet+2);  // X+gamma 
   
-    p->SetCut(cut + " && TConvNumber == 0 && TFakeNumber == 0");
+    p->SetCut(cut + " && TConvNumber == 0 && TFakeNumber == 0"); //Veto Conversions
   	p->AddSample("MuonEG, DoubleEG, DoubleMuon, SingleMuon, SingleElec",                     "Data",  itData);             // Data
     p->AddSample("WZTo3LNu",                                                                   "WZ",       itSignal, kYellow);    // WZ
 
-//  p->AddSample("WWTo2L2Nu, WpWpJJ, WWTo2L2Nu_DoubleScat",         "WW",       itBkg, kBlue+10);  // WW
+    p->AddSample("WWTo2L2Nu, WpWpJJ, WWTo2L2Nu_DoubleScat",         "WW",       itBkg, kBlue+10);  // WW
     p->AddSample("WWW, WWZ, WZZ, ZZZ, VHToNonbb_amcatnlo,WWTo2L2Nu, WpWpJJ, WWTo2L2Nu_DoubleScat",    "VVV/VV",  itBkg, kGreen-9); // RareSM
-/*    p->AddSample("ZZTo4L",    "ZZ",  itBkg, kCyan-5); // RareSM
+    p->AddSample("ZZTo4L",    "ZZ",  itBkg, kCyan-5); // RareSM
     p->AddSample("tZq_ll",    "tZq",  itBkg, kGreen); // RareSM
     p->AddSample("TTZToLL_M1to10,TTZToLLNuNu,TTWToLNu,TTTT,TTHNonbb",    "ttX",  itBkg, kRed-10); // RareSM
-    p->SetCut(cut + " && TConvNumber == 0");
-  	p->AddSample("TW_noFullyHadr,T_tch,Tbar_tch,TbarW_noFullyHadr,TToLeptons_sch_amcatnlo" ,     "Single t", itBkg, kGray);
+    p->SetCut(cut + " && TConvNumber == 0"); //Add the fakes
+  	//p->AddSample("TW_noFullyHadr,T_tch,Tbar_tch,TbarW_noFullyHadr,TToLeptons_sch_amcatnlo" ,     "Single t", itBkg, kGray);
     p->AddSample("TTbar_Powheg, TW_noFullyHadr,T_tch,Tbar_tch,TbarW_noFullyHadr,TToLeptons_sch_amcatnlo", "tt + Single t", itBkg, kRed);
     p->AddSample("WJetsToLNu_MLM", "WJets", itBkg, kOrange-3);
-    p->AddSample("DYJetsToLL_M50_MLM, DYJetsToLL_M5to50_MLM", "DY", itBkg, kOrange);*/
+    p->AddSample("DYJetsToLL_M50_MLM, DYJetsToLL_M5to50_MLM", "DY", itBkg, kOrange);
   }
-    AddFRhisto("fakerate_light_EWKino_Moriond2017_36p5fb.root", "FR_susy_wpM_el_data_comb", "mvaMFRhistoEl");
-    AddFRhisto("fakerate_light_EWKino_Moriond2017_36p5fb.root", "FR_susy_wpM_mu_data_comb", "mvaMFRhistoMu");
+    AddFRhisto("fakerate_light_EWKino_Moriond2017_36p5fb.root", "FR_susy_wpM_el_data_comb", "mvaMFRhistoEl"); //Book Elec Fake Rate
+    AddFRhisto("fakerate_light_EWKino_Moriond2017_36p5fb.root", "FR_susy_wpM_mu_data_comb", "mvaMFRhistoMu"); //Book Muon Fake Rate
   if (NameOfTree != "top"){//Load DATA FAKES, APPLY FAKE RATE FILES!!!
     p->SetCut(cut + " && TConvNumber > 0 && TFakeNumber == 0");
     p->AddSample("TGJets, TTGJets, WGToLNuG, ZGTo2LG, WZG_amcatnlo, WWG_amcatnlo",                "X+#gamma", itBkg, kViolet+2);  // X+gamma 
@@ -77,29 +77,22 @@ void DrawPlot(TString var, TString cut, TString chan, Int_t nbins, Float_t bin0,
     p->AddSample("WWW, WWZ, WZZ, ZZZ, VHToNonbb_amcatnlo,WWTo2L2Nu, WpWpJJ, WWTo2L2Nu_DoubleScat",    "VVV/VV",  itBkg, kGreen-9); // RareSM
     p->AddSample("ZZTo4L",    "ZZ",  itBkg, kCyan-5); // RareSM
     p->AddSample("tZq_ll",    "tZq",  itBkg, kGreen); // RareSM
-    //p->AddSample("TTZToLL_M1to10,TTZToLLNuNu,TTWToLNu,TTTT,TTHNonbb",    "ttX",  itBkg, kRed-10); // RareSM
-      //p->SetCut("TIsSR == 1 && TNFOLeps == 3");
-      //p->SetWeight("GetFRweightlepMVAM3lep(GetFRweightMVAM(TLep_Pt[0], TLep_Eta[0], TIsTight[0], TLep_pdgId[0]), GetFRweightMVAM(TLep_Pt[1], TLep_Eta[1], TIsTight[1], TLep_pdgId[1]), GetFRweightMVAM(TLep_Pt[2], TLep_Eta[2], TIsTight[2], TLep_pdgId[2]), 1)");
-      //p->SetWeight("GetFRweightlepMVAM3lep(GetFRweightMVAM(TLep_Pt[0], TLep_Eta[0], TIsTight[0], TLep_pdgId[0]), GetFRweightMVAM(TLep_Pt[1], TLep_Eta[1], TIsTight[1], TLep_pdgId[1]), GetFRweightMVAM(TLep_Pt[2], TLep_Eta[2], TIsTight[2], TLep_pdgId[2]), 1)");
-      p->SetCut("TIsSR == 1 && TNFOLeps == 3");      
-    //p->SetWeight("GetFRweightlepMVAM3lep(GetFRweightMVAM(TLep_Pt[0], TLep_Eta[0], TIsTight[0], TLep_pdgId[0]), 0.01, 0.01, 1)");
-    p->SetWeight("GetFRweightMVAM(TLep_Pt+1, TLep_Eta+1, TIsTight+1, TLep_pdgId+1)");
-
-    //p->SetWeight("dumpFR(TIsTight[1])");
+    p->AddSample("TTZToLL_M1to10,TTZToLLNuNu,TTWToLNu,TTTT,TTHNonbb",    "ttX",  itBkg, kRed-10); // RareSM
+    cut.ReplaceAll("TNTightLeps", "TNFOLeps");
+    p->SetCut(cut); //Data fakes, relax tight to FO
+    p->SetWeight("GetFRweightlepMVAM3lep(GetFRweightMVAM(TLep_PtZ1, TLep_EtaZ1, TLep_IsTightZ1, TLep_pdgIdZ1), GetFRweightMVAM(TLep_PtZ2, TLep_EtaZ2, TLep_IsTightZ2, TLep_pdgIdZ2), GetFRweightMVAM(TLep_PtW, TLep_EtaW, TLep_IsTightW, TLep_pdgIdW), 1)"); //Fake rates for the fakerate god  
     //DATA FAKES
-
   	p->AddSample("MuonEG, DoubleEG, DoubleMuon, SingleMuon, SingleElec",                     "DataFakes",  itBkg, kBlue,"0", "fake");             // Data
     //PROMPT SUB
-    //p->SetWeight("GetFRweightlepMVAM(TLep_Pt, TLep_Eta, TIsTight, TLep_pdgId,TNTightLeps, true)");
-    //p->SetCut(cut + " && TConvNumber > 0 && TFakeNumber == 0");
-//    p->AddSample("TGJets, TTGJets, WGToLNuG, ZGTo2LG, WZG_amcatnlo, WWG_amcatnlo",                "X+#gamma", itBkg, kViolet+2, "0");  // X+gamma 
-    //p->SetCut(cut + " && TConvNumber == 0 && TFakeNumber == 0");
-    //p->AddSample("WZTo3LNu",                                                                   "WZ",       itSignal, kYellow, "0");    // WZ
-    //p->AddSample("WWTo2L2Nu, WpWpJJ, WWTo2L2Nu_DoubleScat",         "WW",       itBkg, kBlue+10, "0");  // WW
-    //p->AddSample("WWW, WWZ, WZZ, ZZZ, VHToNonbb_amcatnlo",    "VVV/VH",  itBkg, kGreen-9, "0"); // RareSM
-    //p->AddSample("ZZTo4L",    "ZZ",  itBkg, kCyan-5, "0"); // RareSM
-    //p->AddSample("tZq_ll",    "tZq",  itBkg, kGreen, "0"); // RareSM
-    //  p->AddSample("TTZToLL_M1to10,TTZToLLNuNu,TTWToLNu,TTTT,TTHNonbb",    "ttX",  itBkg, kRed-10,"0"); // RareSM
+    p->SetCut(cut + " && TConvNumber > 0 && TFakeNumber == 0");
+    p->AddSample("TGJets, TTGJets, WGToLNuG, ZGTo2LG, WZG_amcatnlo, WWG_amcatnlo",                "X+#gamma", itBkg, kViolet+2, "0");  // X+gamma 
+    p->SetCut(cut + " && TConvNumber == 0 && TFakeNumber == 0");
+    p->AddSample("WZTo3LNu",                                                                   "WZ",       itSignal, kYellow, "0");    // WZ
+    p->AddSample("WWTo2L2Nu, WpWpJJ, WWTo2L2Nu_DoubleScat",         "WW",       itBkg, kBlue+10, "0");  // WW
+    p->AddSample("WWW, WWZ, WZZ, ZZZ, VHToNonbb_amcatnlo",    "VVV/VH",  itBkg, kGreen-9, "0"); // RareSM
+    p->AddSample("ZZTo4L",    "ZZ",  itBkg, kCyan-5, "0"); // RareSM
+    p->AddSample("tZq_ll",    "tZq",  itBkg, kGreen, "0"); // RareSM
+      p->AddSample("TTZToLL_M1to10,TTZToLLNuNu,TTWToLNu,TTTT,TTHNonbb",    "ttX",  itBkg, kRed-10,"0"); // RareSM
   }
 
 
