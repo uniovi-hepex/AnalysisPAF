2b97ae8fdeeac71fa31ef239b95ee7264fefa03e
GonzalezFJR <juan.rodrigo.gonzalez.fernandez@cern.ch>
2017-10-10 11:20:21 +0200

    [Stop] Adding gen info of particles from production process in the minitree

--- packages/StopTopAnalysis/StopTopAnalysis.C
+++ packages/StopTopAnalysis/StopTopAnalysis.C
@@ -23,8 +23,8 @@ void StopTopAnalysis::ResetVariables(){
   TWeight_LepEffUp = 0; TWeight_LepEffDown = 0; TWeight_TrigUp = 0; TWeight_TrigDown = 0;
   TWeight_FSUp = 0; TWeight_FSDown = 0; TWeight_PUDown = 0; TWeight_PUUp = 0;
   TIsSS = false; TNLHEWeight = 0; TISRweight = 0; TISRweightUp = 0; TISRweightDown = 0; TWeight_ISRUp = 0; TWeight_ISRDown = 0;
-  TLep0_Pt = 0; TLep0_Eta = 0; TLep0_Phi = 0; TLep0_E = 0; TLep0_Charge = 0; TLep0_Id = 0;
-  TLep1_Pt = 0; TLep1_Eta = 0; TLep1_Phi = 0; TLep1_E = 0; TLep1_Charge = 0; TLep1_Id = 0;
+  TLep0_Pt = 0; TLep0_Eta = 0; TLep0_Phi = 0; TLep0_E = 0; TLep0_Charge = 0; TLep0_Id = 0; TgenLep0_Mid = 0; 
+  TLep1_Pt = 0; TLep1_Eta = 0; TLep1_Phi = 0; TLep1_E = 0; TLep1_Charge = 0; TLep1_Id = 0; TgenLep1_Mid = 0;
   TgenMETPhi = 0; TNgenLeps = 0;
   TgenLep0_Pt = 0; TgenLep0_Eta = 0; TgenLep1_Pt = 0; TgenLep1_Eta = 0; TgenLep0_Id = 0; TgenLep1_Id = 0;
   TgenDeltaPhi = 0; TgenDeltaEta = 0; TgenMT2 = 0; TNgenJets = 0;  TgenHT = 0;
@@ -48,6 +48,22 @@ void StopTopAnalysis::ResetVariables(){
   TgenTop1Eta = 0;   TgenTop2Eta = 0;
   TgenTop1Phi = 0;   TgenTop2Phi = 0;
   TgenTop1M   = 0;   TgenTop2M   = 0;
+  TgenNeu1Pt  = 0; TgenNeu2Pt  = 0;
+  TgenNeu1Eta = 0; TgenNeu2Eta = 0;
+  TgenNeu1Phi = 0; TgenNeu2Phi = 0;
+  TgenNeu1M   = 0; TgenNeu2M   = 0;
+  TgenW1Pt  = 0; TgenW2Pt  = 0;
+  TgenW1Eta = 0; TgenW2Eta = 0;
+  TgenW1Phi = 0; TgenW2Phi = 0;
+  TgenW1M   = 0; TgenW2M   = 0;
+  TgenStop1Pt  = 0; TgenStop2Pt  = 0;
+  TgenStop1Eta = 0; TgenStop2Eta = 0;
+  TgenStop1Phi = 0; TgenStop2Phi = 0;
+  TgenStop1M   = 0; TgenStop2M   = 0;
+  TgenLsp1Pt  = 0; TgenLsp2Pt  = 0;
+  TgenLsp1Eta = 0; TgenLsp2Eta = 0;
+  TgenLsp1Phi = 0; TgenLsp2Phi = 0;
+  TgenLsp1M   = 0; TgenLsp2M   = 0;
 
   selLeptons.clear(); vetoLeptons.clear();
   genLeptons.clear();
@@ -276,6 +292,8 @@ void StopTopAnalysis::SetLeptonVariables(){
   fTree->Branch("TgenLep1_Id",     &TgenLep1_Id,     "TgenLep1_Id/I");
   fTree->Branch("TgenLep1_Pt",     &TgenLep1_Pt,     "TgenLep1_Pt/F");
   fTree->Branch("TgenLep1_Eta",     &TgenLep1_Eta,     "TgenLep1_Eta/F");
+  fTree->Branch("TgenLep0_Mid" ,    &TgenLep0_Mid ,    "TgenLep0_Mid/I");
+  fTree->Branch("TgenLep1_Mid" ,     &TgenLep1_Mid ,     "TgenLep1_Mid/I");
   fTree->Branch("TgenDeltaPhi",      &TgenDeltaPhi,      "TgenDeltaPhi/F");
   fTree->Branch("TgenDeltaEta",      &TgenDeltaEta,      "TgenDeltaPhi/F");
   fTree->Branch("TgenMT2",      &TgenMT2,      "TgenMT2/F");
@@ -360,6 +378,43 @@ void StopTopAnalysis::SetEventVariables(){
   fTree->Branch("TgenTop2Eta" , &TgenTop2Eta, "TgenTop2Eta/F"); 
   fTree->Branch("TgenTop2Phi" , &TgenTop2Phi, "TgenTop2Phi/F"); 
   fTree->Branch("TgenTop2M"   , &TgenTop2M  , "TgenTop2M/F"  ); 
+
+  fTree->Branch("TnGenW",         &TnGenW,         "TnGenW/I");
+  fTree->Branch("TnGenStop",         &TnGenStop,         "TnGenStop/I");
+  fTree->Branch("TnGenLsp",         &TnGenLsp,         "TnGenLsp/I");
+  fTree->Branch("TnGenNeutrinos",         &TnGenNeutrinos,         "TnGenNeutrinos/I");
+  fTree->Branch("TgenNeu1Pt"  , &TgenNeu1Pt , "TgenNeu1Pt/F" );
+  fTree->Branch("TgenNeu1Eta" , &TgenNeu1Eta, "TgenNeu1Eta/F"); 
+  fTree->Branch("TgenNeu1Phi" , &TgenNeu1Phi, "TgenNeu1Phi/F"); 
+  fTree->Branch("TgenNeu1M"   , &TgenNeu1M  , "TgenNeu1M/F"  ); 
+  fTree->Branch("TgenNeu2Pt"  , &TgenNeu2Pt , "TgenNeu2Pt/F" ); 
+  fTree->Branch("TgenNeu2Eta" , &TgenNeu2Eta, "TgenNeu2Eta/F"); 
+  fTree->Branch("TgenNeu2Phi" , &TgenNeu2Phi, "TgenNeu2Phi/F"); 
+  fTree->Branch("TgenNeu2M"   , &TgenNeu2M  , "TgenNeu2M/F"  ); 
+  fTree->Branch("TgenW1Pt"  , &TgenW1Pt , "TgenW1Pt/F" );
+  fTree->Branch("TgenW1Eta" , &TgenW1Eta, "TgenW1Eta/F"); 
+  fTree->Branch("TgenW1Phi" , &TgenW1Phi, "TgenW1Phi/F"); 
+  fTree->Branch("TgenW1M"   , &TgenW1M  , "TgenW1M/F"  ); 
+  fTree->Branch("TgenW2Pt"  , &TgenW2Pt , "TgenW2Pt/F" ); 
+  fTree->Branch("TgenW2Eta" , &TgenW2Eta, "TgenW2Eta/F"); 
+  fTree->Branch("TgenW2Phi" , &TgenW2Phi, "TgenW2Phi/F"); 
+  fTree->Branch("TgenW2M"   , &TgenW2M  , "TgenW2M/F"  ); 
+  fTree->Branch("TgenStop1Pt"  , &TgenStop1Pt , "TgenStop1Pt/F" );
+  fTree->Branch("TgenStop1Eta" , &TgenStop1Eta, "TgenStop1Eta/F"); 
+  fTree->Branch("TgenStop1Phi" , &TgenStop1Phi, "TgenStop1Phi/F"); 
+  fTree->Branch("TgenStop1M"   , &TgenStop1M  , "TgenStop1M/F"  ); 
+  fTree->Branch("TgenStop2Pt"  , &TgenStop2Pt , "TgenStop2Pt/F" ); 
+  fTree->Branch("TgenStop2Eta" , &TgenStop2Eta, "TgenStop2Eta/F"); 
+  fTree->Branch("TgenStop2Phi" , &TgenStop2Phi, "TgenStop2Phi/F"); 
+  fTree->Branch("TgenStop2M"   , &TgenStop2M  , "TgenStop2M/F"  ); 
+  fTree->Branch("TgenLsp1Pt"  , &TgenLsp1Pt , "TgenLsp1Pt/F" );
+  fTree->Branch("TgenLsp1Eta" , &TgenLsp1Eta, "TgenLsp1Eta/F"); 
+  fTree->Branch("TgenLsp1Phi" , &TgenLsp1Phi, "TgenLsp1Phi/F"); 
+  fTree->Branch("TgenLsp1M"   , &TgenLsp1M  , "TgenLsp1M/F"  ); 
+  fTree->Branch("TgenLsp2Pt"  , &TgenLsp2Pt , "TgenLsp2Pt/F" ); 
+  fTree->Branch("TgenLsp2Eta" , &TgenLsp2Eta, "TgenLsp2Eta/F"); 
+  fTree->Branch("TgenLsp2Phi" , &TgenLsp2Phi, "TgenLsp2Phi/F"); 
+  fTree->Branch("TgenLsp2M"   , &TgenLsp2M  , "TgenLsp2M/F"  ); 
 }
 
 void StopTopAnalysis::GetLeptonVariables(std::vector<Lepton> selLeptons, std::vector<Lepton> VetoLeptons){
@@ -519,10 +574,12 @@ void StopTopAnalysis::GetGenInfo(){
   if(TNgenLeps >= 2){
     TgenLep0_Pt     = genLeptons.at(0).Pt();    
     TgenLep0_Eta    = genLeptons.at(0).Eta();    
+    TgenLep0_Id     = genLeptons.at(0).isMuon? 13 : 11;
+    TgenLep0_Mid    = genLeptons.at(0).Mid;
     TgenLep1_Pt     = genLeptons.at(1).Pt();    
     TgenLep1_Eta    = genLeptons.at(1).Eta();    
-    TgenLep0_Id     = genLeptons.at(0).isMuon? 13 : 11;
-    TgenLep1_Id     = genLeptons.at(0).isMuon? 13 : 11;
+    TgenLep1_Id     = genLeptons.at(1).isMuon? 13 : 11;
+    TgenLep1_Mid    = genLeptons.at(1).Mid;
     TgenDeltaPhi = (genLeptons.at(0).p.DeltaPhi(genLeptons.at(1).p));
     TgenDeltaEta = TMath::Abs(TMath::Abs(genLeptons.at(0).p.Eta()) - TMath::Abs(genLeptons.at(1).p.Eta()));
     TgenMT2 = getMT2ll(genLeptons.at(0), genLeptons.at(1), TgenMET,     Get<Float_t>("met_genPhi"));
@@ -539,6 +596,85 @@ void StopTopAnalysis::GetGenInfo(){
       TgenJet_Eta[i]    = 0;
     }
     TgenHT += TgenJet_Pt[i];
+  }
+
+  TnGenW = 0; TnGenStop = 0; TnGenLsp = 0; TnGenNeutrinos = 0;
+
+  Int_t nGenPart = Get<Int_t>("nGenPart");
+  Int_t id = 0; Int_t mid = 0;
+  for(Int_t i = 0; i < nGenPart; i++){
+    id = TMath::Abs(Get<Int_t>("GenPart_pdgId",i));
+    //>>> This is a neutrino
+    if(id == 12 || id == 14 || id == 16){
+      mid = TMath::Abs(Get<Int_t>("GenPart_motherId",i));
+      if(mid == 23 || mid == 24){ // From W/Z
+        TnGenNeutrinos++;
+        if(TnGenNeutrinos == 1){
+          TgenNeu1Pt  = Get<Float_t>("GenPart_pt"  , i);   
+          TgenNeu1Eta = Get<Float_t>("GenPart_eta" , i);
+          TgenNeu1Phi = Get<Float_t>("GenPart_phi" , i);
+          TgenNeu1M   = Get<Float_t>("GenPart_mass", i);
+        }
+        else if(TnGenNeutrinos >=2){
+          TgenNeu2Pt  = Get<Float_t>("GenPart_pt"  , i);   
+          TgenNeu2Eta = Get<Float_t>("GenPart_eta" , i);
+          TgenNeu2Phi = Get<Float_t>("GenPart_phi" , i);
+          TgenNeu2M   = Get<Float_t>("GenPart_mass", i);
+        }
+      }
+    }
+ 
+    //>>> This is a W
+    if(id == 24){
+      mid = TMath::Abs(Get<Int_t>("GenPart_motherId",i));
+      if(mid == 6 || mid == 1000006){ // From top/stop
+        TnGenW++;
+        if(TnGenW == 1){
+          TgenW1Pt  = Get<Float_t>("GenPart_pt"  , i);   
+          TgenW1Eta = Get<Float_t>("GenPart_eta" , i);
+          TgenW1Phi = Get<Float_t>("GenPart_phi" , i);
+          TgenW1M   = Get<Float_t>("GenPart_mass", i);
+        }
+        else if(TnGenW >=2){
+          TgenW2Pt  = Get<Float_t>("GenPart_pt"  , i);   
+          TgenW2Eta = Get<Float_t>("GenPart_eta" , i);
+          TgenW2Phi = Get<Float_t>("GenPart_phi" , i);
+          TgenW2M   = Get<Float_t>("GenPart_mass", i);
+        }
+      }
+    }
+    //>>> This is a stop 
+    else if(id == 1000006){ 
+      TnGenStop++;
+      if(TnGenStop == 1){
+        TgenStop1Pt  = Get<Float_t>("GenPart_pt"  , i);   
+        TgenStop1Eta = Get<Float_t>("GenPart_eta" , i);
+        TgenStop1Phi = Get<Float_t>("GenPart_phi" , i);
+        TgenStop1M   = Get<Float_t>("GenPart_mass", i);
+      }
+      else if(TnGenStop >=2){
+        TgenStop2Pt  = Get<Float_t>("GenPart_pt"  , i);   
+        TgenStop2Eta = Get<Float_t>("GenPart_eta" , i);
+        TgenStop2Phi = Get<Float_t>("GenPart_phi" , i);
+        TgenStop2M   = Get<Float_t>("GenPart_mass", i);
+      }
+    }
+    //>>> This is a neutralino
+    else if(id == 1000022){
+      TnGenLsp++;
+      if(TnGenLsp == 1){
+        TgenLsp1Pt  = Get<Float_t>("GenPart_pt"  , i);   
+        TgenLsp1Eta = Get<Float_t>("GenPart_eta" , i);
+        TgenLsp1Phi = Get<Float_t>("GenPart_phi" , i);
+        TgenLsp1M   = Get<Float_t>("GenPart_mass", i);
+      }
+      else if(TnGenLsp >=2){
+        TgenLsp2Pt  = Get<Float_t>("GenPart_pt"  , i);   
+        TgenLsp2Eta = Get<Float_t>("GenPart_eta" , i);
+        TgenLsp2Phi = Get<Float_t>("GenPart_phi" , i);
+        TgenLsp2M   = Get<Float_t>("GenPart_mass", i);
+      }
+    }
   }
   return; 
 }
