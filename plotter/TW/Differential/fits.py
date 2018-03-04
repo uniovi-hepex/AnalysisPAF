ac8ad3feca29223ec116146785d8ea32359324b8
sscruz <s.scruz.312@gmail.com>
2018-03-04 19:31:18 +0100

    Adding differential scripts

---
+++ plotter/TW/Differential/fits.py
@@ -0,0 +1,37 @@
+import fitter
+import ROOT
+import array
+import math
+
+
+expected = True
+
+count = 0
+bins = [0,40,80,120]
+
+histo = ROOT.TH1F("histo","",3, array.array('d',bins))
+
+
+for binDn,binUp in zip(bins, bins[1:]):
+    count = count + 1 
+    fit = fitter.FittingSuite("~/TW_differential/AnalysisPAF/plotter/TW/inputs/forCards_Test_%d.root"%count, expected)
+    fit.doAllFits()
+    results = fit.results
+
+    content = results['']
+    err = 0
+    for key in results: 
+        if key == 'obs' or key == '' or 'statbin' in key or 'Down' in key: continue
+        maxDiff = max(map(abs, [results['']-results[key], results['']-results[key.replace('Up','Down')]]))
+        err = err + maxDiff**2
+        #print key.replace('Up',''), maxDiff/results['']
+    
+    print '#',fit.pmap['']['tW'].Integral(), content, math.sqrt(err)
+
+    histo.SetBinContent( histo.FindBin( (binDn+binUp)/2), fit.pmap['']['tW'].Integral()*content       )
+    histo.SetBinError  ( histo.FindBin( (binDn+binUp)/2), fit.pmap['']['tW'].Integral()*math.sqrt(err))
+
+
+out = ROOT.TFile('output.root','recreate')
+histo.Write()
+out.Close()
---
+++ plotter/TW/Differential/getBinning.py
@@ -0,0 +1,57 @@
+import ROOT
+import numpy as np
+nq = 10
+xq=np.zeros(nq)
+yq=np.zeros(nq)
+tf = ROOT.TFile('/nfs/fanae/user/sscruz/TW_jun4/AnalysisPAF/TW_temp/Tree_TW.root')
+tree = tf.Mini1j1t
+
+Base='''
+#ifndef ADDSTUFF_H
+#define ADDSTUFF_H
+#include<iostream>
+using namespace std;
+'''
+
+
+for i in range(0,nq): 
+    xq[i] = float(i+1)/nq
+
+
+var = "TLep1Pt"
+varName = 'LeadingLep'
+bins = [0,50,100,150]
+
+count = 0
+
+for binDn,binUp in zip(bins,bins[1:]):
+    count = count + 1
+    varBin = "{var} > {binDn} && {var} < {binUp}".format(var=var, binUp=binUp, binDn=binDn)
+    tree.Draw("TBDT","TWeight*(TNJets == 1 && TNBtags == 1 && TChannel == 1 && %s)"%varBin)
+    tree.GetHistogram().GetQuantiles(nq,yq,xq)
+
+    Base = Base + '''\n /////////////////////// \n 
+Float_t theBDt_bin%d(Double_t BDT){
+  if      (BDT < %f   ) return 1;
+'''%(count, yq[0])
+
+    subBin = 2
+
+    for i in range(1,nq-1): 
+        Base = Base + '  else if (BDT < %f) return %d;\n'%(yq[i],subBin)
+        subBin = subBin + 1
+
+    Base = Base + '  else                       return %d;\n} \n'%subBin
+
+Base = Base + '#endif'
+
+output = open(varName+'.C','w')
+output.write(Base)
+output.close()
+
+ROOT.gROOT.LoadMacro(varName+'.C+')
+
+for k in range(1, len(bins)):
+    if not hasattr(ROOT, 'theBDt_bin%d'%k): print 'Something went wrong'
+    else: print 'Bin', k, 'ok'
+
