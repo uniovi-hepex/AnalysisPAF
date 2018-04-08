3d9da373b242317480af4c910f57baf1e2ff983f
Adeik <15350389+vrbouza@users.noreply.github.com>
2018-04-08 16:09:45 +0200

    Small change that removes some warnings due to different order in the constructor's argument list and the initialization of those arguments.

--- packages/Lepton/Lepton.h
+++ packages/Lepton/Lepton.h
@@ -41,12 +41,12 @@ class Lepton : public TObject{
     ~Lepton(){};
     TLorentzVector p;
     Int_t charge;
+    Int_t type;
     Int_t decayMode;
     Int_t idDecayMode;
     Int_t idMVA;
     Int_t idAntiE;
     Int_t idAntiMu;
-    Int_t type;
     Int_t Mid; // mother Id, for gen leptons
     Bool_t isElec;
     Bool_t isMuon;
