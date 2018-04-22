90833e55b9db4676e97dc630922b8baf3c9f8996
Adeik <15350389+vrbouza@users.noreply.github.com>
2018-04-23 01:30:32 +0200

    Names of resulting histograms, attemps in resolving the @GonzalezFJR s libraries problem of never ending the execution of any script made, and bugfix related with variable.

--- plotter/TW/Differential/getCards.py
+++ plotter/TW/Differential/getCards.py
@@ -1,31 +1,57 @@
 import ROOT 
 import varList
-import sys
+import sys, os
+print "===== BDT's histograms procedure"
 
-pathToTree = "/nfs/fanae/user/vrbouza/Storage/TW/MiniTrees/19_04_2018/";
+storagepath = "/nfs/fanae/user/vrbouza/Storage/TW/MiniTrees/"
+pathToTree    = ""
+
+def GetLastFolder(stpth):
+    savefolders   = next(os.walk(stpth))[1]
+    saveyears     = map(int, [i[6:]  for i in savefolders])
+    savefolders   = [i for i in savefolders if int(i[6:]) == max(saveyears)]
+    savemonths    = map(int, [i[3:5] for i in savefolders])
+    savefolders   = [i for i in savefolders if int(i[3:5]) == max(savemonths)]
+    savedays      = map(int, [i[:2]  for i in savefolders])
+    savefolders   = [i for i in savefolders if int(i[:2]) == max(savedays)]
+    return (stpth + savefolders[0] + "/")
+    
+
+if (len(sys.argv) > 1):
+    varName     = sys.argv[1]
+    print "> Chosen variable:", varName, "\n"
+    if (len(sys.argv) > 2):
+      pathToTree    = storagepath + sys.argv[2] + "/"
+    else:
+      pathToTree    = GetLastFolder(storagepath)
+    print "> Minitrees will be read from:", pathToTree, "\n"
+else:
+    print "> Default choice of variable and minitrees\n"
+    varName     = 'LeadingJetPt'
+    minipath    = '/nfs/fanae/user/vrbouza/Storage/TW/MiniTrees/19_04_2018/'
+
 NameOfTree = "Mini1j1t";
-
-varName = sys.argv[1]
 
 ROOT.gROOT.SetBatch(True)
 ROOT.gROOT.LoadMacro('../../Histo.C+')
 ROOT.gROOT.LoadMacro('../../Looper.C+')
 ROOT.gROOT.LoadMacro('../../Plot_sub.C+')
-ROOT.gROOT.ProcessLine('.L temp/'+varName+'.C+')
-print 'Loaded temp/'+varName+'.C'
+ROOT.gROOT.ProcessLine('.L temp/' + varName + '.C+')
+print '> Succesfully loaded binning information from temp/' + varName + '.C', "\n"
 
-print ROOT.theBDt_bin1
+#print ROOT.theBDt_bin1
 
 indx = 0
-
 binning = varList.varList[varName]['recobinning']
+print "> Beginning to produce histograms", "\n"
 
 for binDn,binUp in zip(binning, binning[1:]):
     indx = indx+1
-    p = ROOT.Plot_sub(ROOT.TString('theBDt_bin%d( TBDT )'%indx), ROOT.TString('(TIsSS == 0 && TNJets == 1  && TNBtags == 1 && TLeadingJetPt >= %4.2f  && TLeadingJetPt < %4.2f )'%(binDn, binUp)), ROOT.TString('ElMu'), 10, ROOT.Double(0.5), ROOT.Double(10.5), ROOT.TString(varName + '_%d'%indx), ROOT.TString(''))
+    p = ROOT.Plot_sub(ROOT.TString('theBDt_bin%d( TBDT )'%indx), ROOT.TString('(TIsSS == 0 && TNJets == 1  && TNBtags == 1 && T%s >= %4.2f  && T%s < %4.2f )'%(varName, binDn, varName, binUp)), ROOT.TString('ElMu'), 10, ROOT.Double(0.5), ROOT.Double(10.5), ROOT.TString(varName + '_%d'%indx), ROOT.TString(''))
     p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
     p.SetLimitFolder("../../TW/Differential/temp/");
     p.SetPathSignal(pathToTree);
+    p.verbose = True
 
     p.AddSample("WZ",                          "VVttV", ROOT.itBkg, 390);
     p.AddSample("WW",                          "VVttV", ROOT.itBkg);
@@ -57,6 +83,10 @@ for binDn,binUp in zip(binning, binning[1:]):
     p.AddSymmetricHisto("tW",    "JERUp");
 
     p.NoShowVarName = True;
+    p.SetOutputName("forCards_" + varName + '_%d'%indx);
     p.SaveHistograms();
     del p
-                  
+    p = None
+
+print "> Done!", "\n"
+sys.exit()
