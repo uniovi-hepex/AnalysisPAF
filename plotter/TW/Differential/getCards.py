23399324559ad537f47c0b5967bef91b58ee13ca
Adeik <15350389+vrbouza@users.noreply.github.com>
2018-04-25 23:06:32 +0200

    Added all the remain uncertanties to the getCards.py file.

--- plotter/TW/Differential/getCards.py
+++ plotter/TW/Differential/getCards.py
@@ -53,40 +53,80 @@ for binDn,binUp in zip(binning, binning[1:]):
     p.SetPathSignal(pathToTree);
     p.verbose = True
 
-    p.AddSample("WZ",                          "VVttV", ROOT.itBkg, 390);
-    p.AddSample("WW",                          "VVttV", ROOT.itBkg);
-    p.AddSample("ZZ",                          "VVttV", ROOT.itBkg);
-    p.AddSample("TTWToLNu",                    "VVttV", ROOT.itBkg);
-    p.AddSample("TTWToQQ" ,                    "VVttV", ROOT.itBkg);
-    p.AddSample("TTZToQQ" ,                    "VVttV", ROOT.itBkg);
-    p.AddSample("TTZToLLNuNu",                 "VVttV", ROOT.itBkg);
+    p.AddSample("WZ",                           "VVttV", ROOT.itBkg, 390);
+    p.AddSample("WW",                           "VVttV", ROOT.itBkg);
+    p.AddSample("ZZ",                           "VVttV", ROOT.itBkg);
+    p.AddSample("TTWToLNu",                     "VVttV", ROOT.itBkg);
+    p.AddSample("TTWToQQ" ,                     "VVttV", ROOT.itBkg);
+    p.AddSample("TTZToQQ" ,                     "VVttV", ROOT.itBkg);
+    p.AddSample("TTZToLLNuNu",                  "VVttV", ROOT.itBkg);
 
-    p.AddSample("DYJetsToLL_M10to50_aMCatNLO", "DY",    ROOT.itBkg, 852)
-    p.AddSample("DYJetsToLL_M50_aMCatNLO",     "DY",    ROOT.itBkg);
-    p.AddSample("TTbar_Powheg",                "ttbar", ROOT.itBkg, 633)
-    p.AddSample("TW",                          "tW",    ROOT.itBkg, 2)
-    p.AddSample("TbarW",                       "tW",    ROOT.itBkg);
-    p.AddSample("TTbar_PowhegSemi",            "Fakes", ROOT.itBkg, 413)
-    p.AddSample("WJetsToLNu_MLM",              "Fakes", ROOT.itBkg)
+    p.AddSample("DYJetsToLL_M10to50_aMCatNLO",  "DY",    ROOT.itBkg, 852)
+    p.AddSample("DYJetsToLL_M50_aMCatNLO",      "DY",    ROOT.itBkg);
+    p.AddSample("TTbar_Powheg",                 "ttbar", ROOT.itBkg, 633)
+    p.AddSample("TW",                           "tW",    ROOT.itBkg, 2)
+    p.AddSample("TbarW",                        "tW",    ROOT.itBkg);
+    p.AddSample("TTbar_PowhegSemi",             "Fakes", ROOT.itBkg, 413)
+    p.AddSample("WJetsToLNu_MLM",               "Fakes", ROOT.itBkg)
     
-    p.AddSample("TTbar_Powheg",                "ttbar", ROOT.itSys, 1, "JERUp"); 
-    p.AddSample("TW"   ,                       "tW",    ROOT.itSys, 1, "JERUp");
-    p.AddSample("TbarW",                       "tW",    ROOT.itSys, 1, "JERUp");
-
-    p.AddSample("MuonEG",                      "Data",  ROOT.itData);
-    p.AddSample("SingleMuon",                  "Data",  ROOT.itData);
-    p.AddSample("SingleElec",                  "Data",  ROOT.itData);
-
+    p.AddSample("MuonEG",                       "Data",  ROOT.itData);
+    p.AddSample("SingleMuon",                   "Data",  ROOT.itData);
+    p.AddSample("SingleElec",                   "Data",  ROOT.itData);
+    
+    p.AddSample("TW",                           "tW",    ROOT.itSys, 1, "JERUp");
+    p.AddSample("TW_noFullyHadr_isrUp",         "tW",    ROOT.itSys, 1, "isrUp");
+    p.AddSample("TW_noFullyHadr_isrDown",       "tW",    ROOT.itSys, 1, "isrDown");
+    p.AddSample("TW_noFullyHadr_fsrUp",         "tW",    ROOT.itSys, 1, "fsrUp");
+    p.AddSample("TW_noFullyHadr_fsrDown",       "tW",    ROOT.itSys, 1, "fsrDown");
+    p.AddSample("TW_noFullyHadr_MEscaleUp",     "tW",    ROOT.itSys, 1, "tWMe_s_caleUp");
+    p.AddSample("TW_noFullyHadr_MEscaleDown",   "tW",    ROOT.itSys, 1, "tWMe_s_caleDown");
+    p.AddSample("TW_noFullyHadr_PSscaleUp",     "tW",    ROOT.itSys, 1, "tWPS_s_caleUp");
+    p.AddSample("TW_noFullyHadr_PSscaleDown",   "tW",    ROOT.itSys, 1, "tWPS_s_caleDown");
+    
+    p.AddSample("TbarW",                        "tW",    ROOT.itSys, 1, "JERUp");
+    p.AddSample("TbarW_noFullyHadr_isrUp",      "tW",    ROOT.itSys, 1, "isrUp");
+    p.AddSample("TbarW_noFullyHadr_isrDown",    "tW",    ROOT.itSys, 1, "isrDown");
+    p.AddSample("TbarW_noFullyHadr_fsrUp",      "tW",    ROOT.itSys, 1, "fsrUp");
+    p.AddSample("TbarW_noFullyHadr_fsrDown",    "tW",    ROOT.itSys, 1, "fsrDown");
+    p.AddSample("TbarW_noFullyHadr_MEscaleUp",  "tW",    ROOT.itSys, 1, "tWMe_s_caleUp");
+    p.AddSample("TbarW_noFullyHadr_MEscaleDown","tW",    ROOT.itSys, 1, "tWMe_s_caleDown");
+    p.AddSample("TbarW_noFullyHadr_PSscaleUp"  ,"tW",    ROOT.itSys, 1, "tWPS_s_caleUp");
+    p.AddSample("TbarW_noFullyHadr_PSscaleDown","tW",    ROOT.itSys, 1, "tWPS_s_caleDown");
+    
+    p.AddSample("TW_noFullyHadr_DS",            "tW",    ROOT.itSys, 1, "DSUp");
+    p.AddSample("TbarW_noFullyHadr_DS",         "tW",    ROOT.itSys, 1, "DSUp");
+    p.AddSymmetricHisto("tW",  "DSUp");
+    p.AddSymmetricHisto("tW",  "JERUp");
+    
+    p.AddSample("TTbar_Powheg",                 "ttbar", ROOT.itSys, 1, "JERUp");
+    p.AddSample("TTbar_Powheg",                 "ttbar", ROOT.itSys, 1, "ScaleUp");
+    p.AddSample("TTbar_Powheg",                 "ttbar", ROOT.itSys, 1, "ScaleDown");
+    p.AddSample("TTbar_Powheg",                 "ttbar", ROOT.itSys, 1, "pdfUp");
+    p.AddSample("TTbar_Powheg",                 "ttbar", ROOT.itSys, 1, "pdfDown");
+    p.AddSample("TTbar_Powheg_hdampUp",         "ttbar", ROOT.itSys, 1, "hdampUp");
+    p.AddSample("TTbar_Powheg_hdampDown",       "ttbar", ROOT.itSys, 1, "hdampDown");
+    p.AddSample("TTbar_Powheg_ueUp",            "ttbar", ROOT.itSys, 1, "ueUp");
+    p.AddSample("TTbar_Powheg_ueDown",          "ttbar", ROOT.itSys, 1, "ueDown");
+    p.AddSample("TTbar_Powheg_isrUp",           "ttbar", ROOT.itSys, 1, "isrUp");
+    p.AddSample("TTbar_Powheg_isrDown",         "ttbar", ROOT.itSys, 1, "isrDown");
+    p.AddSample("TTbar_Powheg_fsrUp",           "ttbar", ROOT.itSys, 1, "fsrUp");
+    p.AddSample("TTbar_Powheg_fsrDown",         "ttbar", ROOT.itSys, 1, "fsrDown");
+    p.AddSample("TTbar_GluonMoveCRTune",        "ttbar", ROOT.itSys, 1, "GluonMoveCRTune");
+    p.AddSample("TTbar_GluonMoveCRTune_erdON",  "ttbar", ROOT.itSys, 1, "GluonMoveCRTune_erdON");
+    p.AddSample("TTbar_Powheg_erdON",           "ttbar", ROOT.itSys, 1, "Powheg_erdON");
+    p.AddSample("TTbar_QCDbasedCRTune_erdON",   "ttbar", ROOT.itSys, 1, "QCDbasedCRTune_erdON";
+    p.UseEnvelope("ttbar", "GluonMoveCRTune,GluonMoveCRTune_erdON,Powheg_erdON,QCDbasedCRTune_erdON", "ColorReconnection");
+    p.AddSymmetricHisto("ttbar",  "JERUp");
+    
+    # NOTE: statistic uncertanties are plugged inside the fitter under "fit" unc.
+    #       category. This uncertanties are provided by combine in specific bins.
+    #       The rest of the uncertanties must be plugged here (or through the 
+    #       "AddSample" format).
     p.AddSystematic("JES,Btag,Mistag,PU,ElecEff,MuonEff,Trig")
-  
-    p.AddSymmetricHisto("ttbar", "JERUp");
-    p.AddSymmetricHisto("tW",    "JERUp");
 
     p.NoShowVarName = True;
     p.SetOutputName("forCards_" + varName + '_%d'%indx);
     p.SaveHistograms();
     del p
-    p = None
 
 print "> Done!", "\n"
-sys.exit()
