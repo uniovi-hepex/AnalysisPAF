8bb3727ecb2f0ce85a2bcd16d5659ff908a7a82d
Adeik <15350389+vrbouza@users.noreply.github.com>
2018-05-08 16:11:40 +0200

    Implementation of all-variables ONLY unfolding (not complete procedure, i.e., not fitting, not obtaining cards: only unfolding). It is compulsory for that to put as  AllUnf and as  copy. It is not possible to NOT put a second argument, though I now realize that you might not put copy as , but whatever else, preventing that way the copy to the www-related folder. Hmm...

--- plotter/TW/Differential/ExecuteUnfolding.sh
+++ plotter/TW/Differential/ExecuteUnfolding.sh
@@ -9,47 +9,69 @@ else
   echo " "
   variable="LeadingJetPt"
 fi
-echo "> Beggining full unfolding procedure of the variable"
-echo $variable
-echo " "
-echo "> Creating (if they do not exist yet) the temporal and results folders"
-echo " "
-mkdir -p temp
-mkdir -p results
-
-if [ ! -e "temp/UnfoldingInfo.root" ]; then
-  echo "%%%%% FATAL ERROR: the ROOT file with the histograms of the response matrix do not exist."
-  echo "%%%%% This bash script will now end."
-  exit
-else
-  echo "> ROOT file with the histograms of the response matrix found."
+if [ $3 == "onlyunf" ]; then
+  echo "> Beggining ONLY unfolding procedure of the variable"
+  echo $variable
   echo " "
+  echo "> Creating (if they do not exist yet) the temporal and results folders"
+  echo " "
+  mkdir -p temp
+  mkdir -p results
+
+  if [ ! -e "temp/UnfoldingInfo.root" ]; then
+    echo "%%%%% FATAL ERROR: the ROOT file with the histograms of the response matrix do not exist."
+    echo "%%%%% This bash script will now end."
+    exit
+  else
+    echo "> ROOT file with the histograms of the response matrix found."
+    echo " "
+  fi
+
+  echo "> Unfolding chosen variable..."
+  echo " "
+  python UnfoldThings.py $variable
+else
+  echo "> Beggining full unfolding procedure of the variable"
+  echo $variable
+  echo " "
+  echo "> Creating (if they do not exist yet) the temporal and results folders"
+  echo " "
+  mkdir -p temp
+  mkdir -p results
+
+  if [ ! -e "temp/UnfoldingInfo.root" ]; then
+    echo "%%%%% FATAL ERROR: the ROOT file with the histograms of the response matrix do not exist."
+    echo "%%%%% This bash script will now end."
+    exit
+  else
+    echo "> ROOT file with the histograms of the response matrix found."
+    echo " "
+  fi
+
+  # 1) Get the binning in order to obtain the BDT histograms.
+  echo "> Obtaining BDT's binning..."
+  echo " "
+  python getBinning.py $variable 0
+
+  # 2) Get those histograms.
+  echo "> Obtaining histograms per BDT's bin..."
+  echo " "
+  python getCards.py $variable
+
+  # 3) Do a fit in each bin of your histograms and for all the systematics.
+  echo "> Performing fits to each bin of the BDT's distribution..."
+  echo " "
+  cd
+  source pre_start_CMS.sh
+  cd Documents/TFM/AnalysisPAF/plotter/TW/Differential/
+  python fits.py $variable
+
+  # 4) Do a proper unfolding as you were taught by your mother when you were a child.
+  source ../pre_start.sh
+  echo "> Unfolding chosen variable..."
+  echo " "
+  python UnfoldThings.py $variable
 fi
-
-# 1) Get the binning in order to obtain the BDT histograms.
-echo "> Obtaining BDT's binning..."
-echo " "
-python getBinning.py $variable 0
-
-# 2) Get those histograms.
-echo "> Obtaining histograms per BDT's bin..."
-echo " "
-python getCards.py $variable
-
-# 3) Do a fit in each bin of your histograms and for all the systematics.
-echo "> Performing fits to each bin of the BDT's distribution..."
-echo " "
-cd
-source pre_start_CMS.sh
-cd Documents/TFM/AnalysisPAF/plotter/TW/Differential/
-python fits.py $variable
-
-# 4) Do a proper unfolding as you were taught by your mother when you were a child.
-source ../pre_start.sh
-echo "> Unfolding chosen variable..."
-echo " "
-python UnfoldThings.py $variable
-
 
 if [ "$2" == "copy" ]; then
   cd ..
