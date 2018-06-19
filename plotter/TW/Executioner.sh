1c13f53a30761b931b8c1da8bc30007ece312dba
Adeik <15350389+vrbouza@users.noreply.github.com>
2018-06-19 19:23:58 +0200

    Added more extensions to our samples of syst. (and one that we did not add before to background).

--- plotter/TW/Executioner.sh
+++ plotter/TW/Executioner.sh
@@ -3,7 +3,7 @@
 # MLM for DY:
 #
 samples=("TW" "TW_noFullyHadr" 
-  "TbarW" "TbarW_noFullyHadr" 
+  "TbarW" "TbarW_noFullyHadr" "TW_aMCatNLO" 
   "TTbar_Powheg" "TTbar_PowhegSemi" "TTbar2L_powheg" "DYJetsToLL_M50_MLM" "DYJetsToLL_M5to50_MLM" 
   "WJetsToLNu_MLM" "ZZ" "WW" "WZ" "TTWToLNu" "TTZToQQ" "TTZToLLNuNu" "TTWToQQ" "TTGJets" 
   "MuonEG" "SingleElec" "SingleMuon")
@@ -21,8 +21,8 @@ samples=("TW" "TW_noFullyHadr"
 samples_syst=("TTbar_Powheg_ueUp" "TTbar2L_Powheg_ueUp" "TTbar_Powheg_ueDown" "TTbar2L_Powheg_ueDown" 
   "TTbar_Powheg_isrUp" "TTbar_Powheg_isrDown" "TTbar_Powheg_fsrUp" 
   "TTbar_Powheg_fsrDown" "TTbar_Powheg_hdampUp" "TTbar2L_Powheg_hdampUp" 
-  "TTbar_Powheg_hdampDown" "TTbar2L_Powheg_hdampDown" "TTbar_Powheg_erdON" 
-  "TTbar_QCDbasedCRTune_erdON" "TTbar_GluonMoveCRTune" "TTbar_GluonMoveCRTune_erdON" 
+  "TTbar_Powheg_hdampDown" "TTbar2L_Powheg_hdampDown" "TTbar_Powheg_erdON" "TTTo2L2Nu_Powheg_erdON"
+  "TTbar_QCDbasedCRTune_erdON" "TTTo2L2Nu_QCDbasedCRTune_erdON" "TTbar_GluonMoveCRTune" "TTTo2L2Nu_GluonMoveCRTune" "TTbar_GluonMoveCRTune_erdON" 
   "TW_noFullyHadr_isrUp" "TW_noFullyHadr_isrDown" "TW_noFullyHadr_fsrUp" "TW_noFullyHadr_fsrDown" "TW_noFullyHadr_MEscaleUp" "TW_noFullyHadr_MEscaleDown" 
   "TW_noFullyHadr_PSscaleUp" "TW_noFullyHadr_PSscaleDown" "TW_noFullyHadr_DS" 
   "TbarW_noFullyHadr_isrUp" "TbarW_noFullyHadr_isrDown" "TbarW_noFullyHadr_fsrUp" "TbarW_noFullyHadr_fsrDown" "TbarW_noFullyHadr_MEscaleUp" "TbarW_noFullyHadr_MEscaleDown" 
@@ -41,15 +41,15 @@ samples_syst=("TTbar_Powheg_ueUp" "TTbar2L_Powheg_ueUp" "TTbar_Powheg_ueDown" "T
 samples_unf=("UNF_TW" "UNF_TW_noFullyHadr_isrUp" "UNF_TW_noFullyHadr_isrDown" "UNF_TW_noFullyHadr_fsrUp" "UNF_TW_noFullyHadr_fsrDown" "UNF_TW_noFullyHadr_MEscaleUp" "UNF_TW_noFullyHadr_MEscaleDown" 
   "UNF_TW_noFullyHadr_PSscaleUp" "UNF_TW_noFullyHadr_PSscaleDown" "UNF_TW_noFullyHadr_DS" 
   "UNF_TbarW" "UNF_TbarW_noFullyHadr_isrUp" "UNF_TbarW_noFullyHadr_isrDown" "UNF_TbarW_noFullyHadr_fsrUp" "UNF_TbarW_noFullyHadr_fsrDown" "UNF_TbarW_noFullyHadr_MEscaleUp" "UNF_TbarW_noFullyHadr_MEscaleDown" 
-  "UNF_TbarW_noFullyHadr_PSscaleUp" "UNF_TbarW_noFullyHadr_PSscaleDown" "UNF_TbarW_noFullyHadr_DS")
+  "UNF_TbarW_noFullyHadr_PSscaleUp" "UNF_TbarW_noFullyHadr_PSscaleDown" "UNF_TbarW_noFullyHadr_DS" "UNF_TW_aMCatNLO")
 
 
 # MLM for DY:
 #
 runsamples=("TW_ext" "TW_noFullyHadr & TW_noFullyHadr_ext & TW_noFullyHadr_ext2" 
-  "TbarW_ext" "TbarW_noFullyHadr & TbarW_noFullyHadr_ext & TbarW_noFullyHadr_ext2" 
+  "TbarW_ext" "TbarW_noFullyHadr & TbarW_noFullyHadr_ext & TbarW_noFullyHadr_ext2" "TW_aMCatNLO" 
   "TTbar_Powheg" "TTbar_Powheg" "TTbar2L_powheg" "DYJetsToLL_M50_MLM_ext & DYJetsToLL_M50_MLM_ext2" "DYJetsToLL_M5to50_MLM" 
-  "WJetsToLNu_MLM & WJetsToLNu_MLM_ext2" "ZZ & ZZ_ext" "WW & WW_ext" "WZ & WZ_ext" "TTWToLNu_ext1 & TTWToLNu_ext2" "TTZToQQ" "TTZToLLNuNu_ext1 & TTZToLLNuNu_ext2" "TTWToQQ" "TTGJets" 
+  "WJetsToLNu_MLM & WJetsToLNu_MLM_ext2" "ZZ & ZZ_ext" "WW & WW_ext" "WZ & WZ_ext" "TTWToLNu_ext1 & TTWToLNu_ext2" "TTZToQQ" "TTZToLLNuNu_ext1 & TTZToLLNuNu_ext2" "TTWToQQ" "TTGJets & TTGJets_ext" 
   "MuonEG" "SingleElec" "SingleMuon")
 
 # aMC@NLO for DY:
@@ -63,10 +63,10 @@ runsamples=("TW_ext" "TW_noFullyHadr & TW_noFullyHadr_ext & TW_noFullyHadr_ext2"
 # All the samples for syst. uncs.:
 #
 runsamples_syst=("TTbar_Powheg_ueUp & TTbar_Powheg_ueUp_ext" "TTbar2L_Powheg_ueUp" "TTbar_Powheg_ueDown & TTbar_Powheg_ueDown_ext" "TTbar2L_Powheg_ueDown" 
-  "TTbar_Powheg_isrUp_ext" "TTbar_Powheg_isrDown & TTbar_Powheg_isrDown_ext" "TTbar_Powheg_fsrUp & TTbar_Powheg_fsrUp_ext" 
-  "TTbar_Powheg_fsrDown & TTbar_Powheg_fsrDown_ext" "TTbar_Powheg_hdampUp & TTbar_Powheg_hdampUp_ext" "TTbar2L_Powheg_hdampUp" 
-  "TTbar_Powheg_hdampDown & TTbar_Powheg_hdampDown_ext" "TTbar2L_Powheg_hdampDown" "TTbar_Powheg_erdON & TTbar_Powheg_erdON_ext" 
-  "TTbar_QCDbasedCRTune_erdON & TTbar_QCDbasedCRTune_erdON_ext" "TTbar_GluonMoveCRTune" "TTbar_GluonMoveCRTune_erdON" 
+  "TTbar_Powheg_isrUp_ext & TTbar_Powheg_isrUp_ext2" "TTbar_Powheg_isrDown & TTbar_Powheg_isrDown_ext & TTbar_Powheg_isrDown_ext2"
+  "TTbar_Powheg_fsrUp & TTbar_Powheg_fsrUp_ext & TTbar_Powheg_fsrUp_ext2" "TTbar_Powheg_fsrDown & TTbar_Powheg_fsrDown_ext & TTbar_Powheg_fsrDown_ext2" "TTbar_Powheg_hdampUp & TTbar_Powheg_hdampUp_ext" "TTbar2L_Powheg_hdampUp" 
+  "TTbar_Powheg_hdampDown & TTbar_Powheg_hdampDown_ext" "TTbar2L_Powheg_hdampDown" "TTbar_Powheg_erdON & TTbar_Powheg_erdON_ext" "TTTo2L2Nu_Powheg_erdON"
+  "TTbar_QCDbasedCRTune_erdON & TTbar_QCDbasedCRTune_erdON_ext" "TTTo2L2Nu_QCDbasedCRTune_erdON" "TTbar_GluonMoveCRTune" "TTTo2L2Nu_GluonMoveCRTune" "TTbar_GluonMoveCRTune_erdON" 
   "TW_noFullyHadr_isrUp" "TW_noFullyHadr_isrDown" "TW_noFullyHadr_fsrUp" "TW_noFullyHadr_fsrDown" "TW_noFullyHadr_MEscaleUp" "TW_noFullyHadr_MEscaleDown" 
   "TW_noFullyHadr_PSscaleUp" "TW_noFullyHadr_PSscaleDown" "TW_noFullyHadr_DS" 
   "TbarW_noFullyHadr_isrUp" "TbarW_noFullyHadr_isrDown" "TbarW_noFullyHadr_fsrUp" "TbarW_noFullyHadr_fsrDown" "TbarW_noFullyHadr_MEscaleUp" "TbarW_noFullyHadr_MEscaleDown" 
@@ -85,7 +85,7 @@ runsamples_syst=("TTbar_Powheg_ueUp & TTbar_Powheg_ueUp_ext" "TTbar2L_Powheg_ueU
 runsamples_unf=("TW_ext" "TW_noFullyHadr_isrUp" "TW_noFullyHadr_isrDown" "TW_noFullyHadr_fsrUp" "TW_noFullyHadr_fsrDown" "TW_noFullyHadr_MEscaleUp" "TW_noFullyHadr_MEscaleDown" 
   "TW_noFullyHadr_PSscaleUp" "TW_noFullyHadr_PSscaleDown" "TW_noFullyHadr_DS" 
   "TbarW_ext" "TbarW_noFullyHadr_isrUp" "TbarW_noFullyHadr_isrDown" "TbarW_noFullyHadr_fsrUp" "TbarW_noFullyHadr_fsrDown" "TbarW_noFullyHadr_MEscaleUp" "TbarW_noFullyHadr_MEscaleDown" 
-  "TbarW_noFullyHadr_PSscaleUp" "TbarW_noFullyHadr_PSscaleDown" "TbarW_noFullyHadr_DS")
+  "TbarW_noFullyHadr_PSscaleUp" "TbarW_noFullyHadr_PSscaleDown" "TbarW_noFullyHadr_DS" "TW_aMCatNLO")
 
 
 uplimit=$((${#runsamples[@]}-1))
