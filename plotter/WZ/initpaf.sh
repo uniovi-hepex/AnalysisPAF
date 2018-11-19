8cf05b188815ea110be884a812a689f87b3e8c2a
Carlos Erice <cericecid@gmail.com>
2018-11-19 11:46:51 +0100

    Moved initpaf.sh because of @vrbouza's guidelines

---
+++ plotter/WZ/initpaf.sh
@@ -0,0 +1,5 @@
+cd /mnt_pool/ciencias_users/user/carlosec/AnalysisPAF/
+source /cms/slc6_amd64_gcc530/external/gcc/5.3.0/etc/profile.d/init.sh; source /cms/slc6_amd64_gcc530/external/python/2.7.11-giojec2/etc/profile.d/init.sh; source /cms/slc6_amd64_gcc530/external/python/2.7.11-giojec2/etc/profile.d/dependencies-setup.sh; source /cms/slc6_amd64_gcc530/external/cmake/3.5.2/etc/profile.d/init.sh;source /opt/root6/bin/thisroot.sh
+
+source /opt/PAF/PAF_setup.sh
+
