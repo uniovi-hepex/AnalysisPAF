073361d56d94877f7b037c6f5a765e729f0ed596
ireneaj <uo239511@uniovi.es>
2018-12-05 11:57:35 +0100

    Comment code that is not generate (unfolding and sytematics)

--- plotter/TWTTbar/getClosureHistos.py
+++ plotter/TWTTbar/getClosureHistos.py
@@ -23,7 +23,7 @@ if (len(sys.argv) > 1):
 else:
     print "> Default choice of variable and minitrees\n"
     varName     = 'LeadingLepEta'
-    pathToTree  = "../../TWTTbar_temp/"
+    pathToTree  = "../../TWTTbar_temp/"    
 
 
 NameOfTree = "Mini1j1t";
@@ -43,66 +43,66 @@ if not os.path.isdir('temp/{var}'.format(var = varName)):
     os.system('mkdir -p temp/{var}'.format(var = varName))
 print "> Beginning to produce the histograms", "\n"
 
-p = r.PlotToPyC(r.TString(vl.varList[varName]['var_gen']), r.TString(gencut), r.TString('All'), len(binning)-1, binning, r.TString('TGen' + varName), r.TString(''))
-p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
-p.SetLimitFolder('temp/{var}/'.format(var = varName));
-p.SetPathSignal(pathToTree);
-p.verbose = True
-p.verbose = False
-p.SetLumi(vl.Lumi)
+#p = r.PlotToPyC(r.TString(vl.varList[varName]['var_gen']), r.TString(gencut), r.TString('All'), len(binning)-1, binning, r.TString('TGen' + varName), r.TString(''))
+#p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
+#p.SetLimitFolder('temp/{var}/'.format(var = varName));
+#p.SetPathSignal(pathToTree);
+#p.verbose = True
+#p.verbose = False
+#p.SetLumi(vl.Lumi)
 
-#p.AddSample("UNF_TW",    "tW", r.itBkg, 2, '', opts)
-#p.AddSample("UNF_TbarW", "tW", r.itBkg, 2, '', opts)
+##p.AddSample("UNF_TW",    "tW", r.itBkg, 2, '', opts)
+##p.AddSample("UNF_TbarW", "tW", r.itBkg, 2, '', opts)
 
-specialweight = vl.n_tw/vl.sigma_tw/(vl.n_tw/vl.sigma_tw + vl.n_twnohad/vl.sigma_twnohad)
-p.SetWeight('TWeight*' + str(specialweight))
-p.AddSample('UNF_TW',                     'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
-specialweight = vl.n_twnohad/vl.sigma_twnohad/(vl.n_tw/vl.sigma_tw + vl.n_twnohad/vl.sigma_twnohad)
-p.SetWeight('TWeight*' + str(specialweight))
-p.AddSample('UNF_TW_noFullyHadr',         'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
-specialweight = vl.n_tbarw/vl.sigma_tw/(vl.n_tbarw/vl.sigma_tw + vl.n_tbarwnohad/vl.sigma_twnohad)
-p.SetWeight('TWeight*' + str(specialweight))
-p.AddSample('UNF_TbarW',                  'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
-specialweight = vl.n_tbarwnohad/vl.sigma_twnohad/(vl.n_tbarw/vl.sigma_tw + vl.n_tbarwnohad/vl.sigma_twnohad)
-p.SetWeight('TWeight*' + str(specialweight))
-p.AddSample('UNF_TbarW_noFullyHadr',      'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
-p.SetWeight('TWeight')
+#specialweight = vl.n_tw/vl.sigma_tw/(vl.n_tw/vl.sigma_tw + vl.n_twnohad/vl.sigma_twnohad)
+#p.SetWeight('TWeight*' + str(specialweight))
+#p.AddSample('UNF_TW',                     'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
+#specialweight = vl.n_twnohad/vl.sigma_twnohad/(vl.n_tw/vl.sigma_tw + vl.n_twnohad/vl.sigma_twnohad)
+#p.SetWeight('TWeight*' + str(specialweight))
+#p.AddSample('UNF_TW_noFullyHadr',         'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
+#specialweight = vl.n_tbarw/vl.sigma_tw/(vl.n_tbarw/vl.sigma_tw + vl.n_tbarwnohad/vl.sigma_twnohad)
+#p.SetWeight('TWeight*' + str(specialweight))
+#p.AddSample('UNF_TbarW',                  'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
+#specialweight = vl.n_tbarwnohad/vl.sigma_twnohad/(vl.n_tbarw/vl.sigma_tw + vl.n_tbarwnohad/vl.sigma_twnohad)
+#p.SetWeight('TWeight*' + str(specialweight))
+#p.AddSample('UNF_TbarW_noFullyHadr',      'tW',      r.itBkg, r.TColor.GetColor("#ffcc33"), '', opts)
+#p.SetWeight('TWeight')
 
-p.NoShowVarName = True;
-p.SetOutputName("ClosureTest_" + varName);
-p.SaveHistograms();
-del p
+#p.NoShowVarName = True;
+#p.SetOutputName("ClosureTest_" + varName);
+#p.SaveHistograms();
+#del p
 
-p = r.PlotToPyC(r.TString(vl.varList[varName]['var_gen']), r.TString(gencut), r.TString('All'), len(binning)-1, binning, r.TString('TGen' + varName), r.TString(''))
-p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
-p.SetLimitFolder('temp/{var}/'.format(var = varName));
-p.SetPathSignal(pathToTree);
-p.verbose = True
-p.verbose = False
-p.SetLumi(vl.Lumi)
+#p = r.PlotToPyC(r.TString(vl.varList[varName]['var_gen']), r.TString(gencut), r.TString('All'), len(binning)-1, binning, r.TString('TGen' + varName), r.TString(''))
+#p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
+#p.SetLimitFolder('temp/{var}/'.format(var = varName));
+#p.SetPathSignal(pathToTree);
+#p.verbose = True
+#p.verbose = False
+#p.SetLumi(vl.Lumi)
 
-p.AddSample("UNF_TW_aMCatNLO",    "tW", r.itBkg, 2, '', opts)
+#p.AddSample("UNF_TW_aMCatNLO",    "tW", r.itBkg, 2, '', opts)
 
-p.NoShowVarName = True;
-p.SetOutputName("ClosureTest_aMCatNLO_" + varName);
-p.SaveHistograms();
-del p
+#p.NoShowVarName = True;
+#p.SetOutputName("ClosureTest_aMCatNLO_" + varName);
+#p.SaveHistograms();
+#del p
 
-p = r.PlotToPyC(r.TString(vl.varList[varName]['var_gen']), r.TString(gencut), r.TString('All'), len(binning)-1, binning, r.TString('TGen' + varName), r.TString(''))
-p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
-p.SetLimitFolder('temp/{var}/'.format(var = varName));
-p.SetPathSignal(pathToTree);
-p.verbose = True
-p.verbose = False
-p.SetLumi(vl.Lumi)
+#p = r.PlotToPyC(r.TString(vl.varList[varName]['var_gen']), r.TString(gencut), r.TString('All'), len(binning)-1, binning, r.TString('TGen' + varName), r.TString(''))
+#p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
+#p.SetLimitFolder('temp/{var}/'.format(var = varName));
+#p.SetPathSignal(pathToTree);
+#p.verbose = True
+#p.verbose = False
+#p.SetLumi(vl.Lumi)
 
-p.AddSample("UNF_TW_noFullyHadr_DS",    "tW", r.itBkg, 2, '', opts)
-p.AddSample("UNF_TbarW_noFullyHadr_DS", "tW", r.itBkg, 2, '', opts)
+#p.AddSample("UNF_TW_noFullyHadr_DS",    "tW", r.itBkg, 2, '', opts)
+#p.AddSample("UNF_TbarW_noFullyHadr_DS", "tW", r.itBkg, 2, '', opts)
 
-p.NoShowVarName = True;
-p.SetOutputName("ClosureTest_DS_" + varName);
-p.SaveHistograms();
-del p
+#p.NoShowVarName = True;
+#p.SetOutputName("ClosureTest_DS_" + varName);
+#p.SaveHistograms();
+#del p
 
 p = r.PlotToPyC(r.TString(vl.varList[varName]['var']), r.TString(recocut), r.TString('All'), len(binningreco)-1, binningreco, r.TString('T' + varName), r.TString(''))
 p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
@@ -149,20 +149,20 @@ p.SetOutputName("ClosureTest_aMCatNLO_recobinning_" + varName);
 p.SaveHistograms();
 del p
 
-p = r.PlotToPyC(r.TString(vl.varList[varName]['var']), r.TString(recocut), r.TString('All'), len(binningreco)-1, binningreco, r.TString('T' + varName), r.TString(''))
-p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
-p.SetLimitFolder('temp/{var}/'.format(var = varName));
-p.SetPathSignal(pathToTree);
-p.verbose = True
-p.verbose = False
-p.SetLumi(vl.Lumi)
+#p = r.PlotToPyC(r.TString(vl.varList[varName]['var']), r.TString(recocut), r.TString('All'), len(binningreco)-1, binningreco, r.TString('T' + varName), r.TString(''))
+#p.SetPath(pathToTree); p.SetTreeName(NameOfTree);
+#p.SetLimitFolder('temp/{var}/'.format(var = varName));
+#p.SetPathSignal(pathToTree);
+#p.verbose = True
+#p.verbose = False
+#p.SetLumi(vl.Lumi)
 
-p.AddSample("TW_noFullyHadr_DS",    "tW", r.itBkg, 2, '', opts)
-p.AddSample("TbarW_noFullyHadr_DS", "tW", r.itBkg, 2, '', opts)
+#p.AddSample("TW_noFullyHadr_DS",    "tW", r.itBkg, 2, '', opts)
+#p.AddSample("TbarW_noFullyHadr_DS", "tW", r.itBkg, 2, '', opts)
 
-p.NoShowVarName = True;
-p.SetOutputName("ClosureTest_DS_recobinning_" + varName);
-p.SaveHistograms();
-del p
+#p.NoShowVarName = True;
+#p.SetOutputName("ClosureTest_DS_recobinning_" + varName);
+#p.SaveHistograms();
+#del p
 
 print "> Done!", "\n"
