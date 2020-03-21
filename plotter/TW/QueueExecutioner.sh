ca3068753170e11de4ea9049d48c76dd46de742e
Adeik <15350389+vrbouza@users.noreply.github.com>
2020-03-21 13:53:04 +0100

    Enhanced executioners

--- plotter/TW/QueueExecutioner.sh
+++ plotter/TW/QueueExecutioner.sh
@@ -14,6 +14,8 @@ echo ""
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TW ANALYSIS EXECUTION %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo ""
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Creating jobs..."
-An=$(qsub -q proof -l nodes=1:ppn=$1 -l walltime=20:00:00 -o $logpath -e $logpath -d $workingpath -F "an $1 $2 $3" Executioner.sh)
-echo $An
-qsub -q proof -l nodes=1:ppn=$1 -l walltime=20:00:00 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$An -F "ch $1 $2 $3" Executioner.sh
+# An=$(qsub -q proof -l nodes=1:ppn=$1 -l walltime=20:00:00 -o $logpath -e $logpath -d $workingpath -F "an $1 $2 $3" Executioner.sh)
+#echo $An
+# qsub -q proof -l nodes=1:ppn=$1 -l walltime=20:00:00 -o $logpath -e $logpath -d $workingpath -W depend=afterany:$An -F "ch $1 $2 $3" Executioner.sh
+sbatch -p batch -c $1 -J "OldPAF" Executioner.sh "ch" $1 $2 $3
+#sbatch -p short -c $1 -J "OldPAF" Executioner.sh "ch" $1 $2 $3
